f48fb183a0e9dd03b355a8cfcdea5aa85989851a
Thomas Beaujean <thomas@appventus.com>
2014-06-20 14:31:57 +0200

    [VICMS-208] [businessEntitytTemplate] utiliser des onglets pour lister les businessEntityTemplate

--- Bundle/BusinessEntityTemplateBundle/Controller/BusinessEntityController.php
+++ Bundle/BusinessEntityTemplateBundle/Controller/BusinessEntityController.php
@@ -23,52 +23,24 @@ class BusinessEntityController extends BasePageController
     public function indexAction()
     {
         $businessEntityManager = $this->get('victoire_core.helper.business_entity_helper');
+        $businessEntityTemplateHelper = $this->get('victoire_business_entity_template.helper.business_entity_template_helper');
+
+        $businessEntitiesTemplates = array();
 
         $businessEntities = $businessEntityManager->getBusinessEntities();
+
+        foreach ($businessEntities as $businessEntity) {
+            $name = $businessEntity->getName();
+            $templates = $businessEntityTemplateHelper->findTemplatesByBusinessEntity($businessEntity);
+            $businessEntitiesTemplates[$name] = $templates;
+        }
 
         return new JsonResponse(array(
             'html' => $this->container->get('victoire_templating')->render(
                 'VictoireBusinessEntityTemplateBundle:BusinessEntity:index.html.twig',
-                array('businessEntities' => $businessEntities)
-            ),
-            'success' => true
-        ));
-    }
-
-    /**
-     *
-     * @Route("/list/{id}", name="victoire_businessentitytemplate_businessentity_list")
-     *
-     * @param string $id The id of the business entity
-     *
-     * @return template
-     *
-     * @throws Exception If the business entity was not found
-     *
-     */
-    public function listAction($id)
-    {
-        //services
-        $businessEntityManager = $this->get('victoire_core.helper.business_entity_helper');
-        $businessEntityTemplateHelper = $this->get('victoire_business_entity_template.helper.business_entity_template_helper');
-
-        //get the businessEntity
-        $businessEntity = $businessEntityManager->findById($id);
-
-        //test the result
-        if ($businessEntity === null) {
-            throw new \Exception('The business entity ['.$id.'] was not found.');
-        }
-
-        //get the templates associated to the business entity
-        $templates = $businessEntityTemplateHelper->findTemplatesByBusinessEntity($businessEntity);
-
-        $parameters = array('businessEntity' => $businessEntity, 'templates' => $templates);
-
-        return new JsonResponse(array(
-            'html' => $this->container->get('victoire_templating')->render(
-                'VictoireBusinessEntityTemplateBundle:BusinessEntity:list.html.twig',
-                $parameters
+                array('businessEntities' => $businessEntities,
+                    'businessEntitiesTemplates' => $businessEntitiesTemplates
+                )
             ),
             'success' => true
         ));
