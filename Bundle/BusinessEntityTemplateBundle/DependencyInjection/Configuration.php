f6482b0d97628fea9f08fdaa158ac12364becac5
Thomas Beaujean <thomas@appventus.com>
2014-06-25 10:34:28 +0200

    fix: review from Paul

--- Bundle/BusinessEntityTemplateBundle/DependencyInjection/Configuration.php
+++ Bundle/BusinessEntityTemplateBundle/DependencyInjection/Configuration.php
@@ -20,7 +20,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $treeBuilder->root('victoire_page');
+        $treeBuilder->root('victoire_business_entity_template');
 
         return $treeBuilder;
     }
