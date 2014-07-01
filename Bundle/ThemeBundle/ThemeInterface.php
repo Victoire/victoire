1ba8f9392682f3fe9f83033ecbc8556438d62584
Thomas Beaujean <thomas@appventus.com>
2014-07-01 14:47:25 +0200

    [VICMS-258] refactor widget theme

--- Bundle/ThemeBundle/ThemeInterface.php
+++ Bundle/ThemeBundle/ThemeInterface.php
@@ -4,6 +4,11 @@ namespace Victoire\Bundle\ThemeBundle;
 
 use Victoire\Bundle\CoreBundle\Widget\Managers\ManagerInterface;
 
+/**
+ *
+ * @author Paul Andrieux
+ *
+ */
 interface ThemeInterface
 {
 
