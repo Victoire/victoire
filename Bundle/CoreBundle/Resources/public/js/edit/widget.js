a36537c7d55920ece3c71426fabf08d3637ba8ac
lenybernard <leny@appventus.com>
2014-08-19 01:16:50 +0200

    trigger widget.js events (used by ckeditor any way) + fix several edit bug

--- Bundle/CoreBundle/Resources/public/js/edit/widget.js
+++ Bundle/CoreBundle/Resources/public/js/edit/widget.js
@@ -13,13 +13,15 @@ $vic(document).on('click', '.vic-new-widget > .vic-dropdown-menu a', function(ev
 $vic(document).on('click', '.vic-widget-modal *[data-modal="create"]', function(event) {
     event.preventDefault();
     // we remove the prototype picker to avoid persist it
-    if ($("select.picker_entity_select").length != 0 && $("select.picker_entity_select").attr('name').indexOf('[items][__name__][entity]') !== -1) {
-        $("select.picker_entity_select").remove();
+    if ($vic("select.picker_entity_select").length != 0 && $vic("select.picker_entity_select").attr('name').indexOf('[items][__name__][entity]') !== -1) {
+        $vic("select.picker_entity_select").remove();
     }
     //we look for the form currently active and visible
     var form = $vic(this).parents('.vic-modal-content').find('.vic-tab-pane.vic-active form').filter(":visible");
+    $vic(form).trigger("victoire_widget_form_create_presubmit");
 
     loading(true);
+
     $vic.ajax({
         type: form.attr('method'),
         url : form.attr('action'),
@@ -48,6 +50,7 @@ $vic(document).on('click', '.vic-widget-modal *[data-modal="create"]', function(
         console.log(response);
         error('Oups, une erreur est apparue', 10000);
     });
+    $vic(form).trigger("victoire_widget_form_create_postsubmit");
 });
 
 
@@ -56,10 +59,11 @@ $vic(document).on('click', '.vic-widget-modal a[data-modal="update"]', function(
     event.preventDefault();
 
     // we remove the prototype picker to avoid persist it
-    if ($("select.picker_entity_select").length != 0 && $("select.picker_entity_select").attr('name').indexOf('appventus_victoirecorebundle_widgetlistingtype[items][__name__][entity]') !== -1) {
-        $("select.picker_entity_select").remove();
+    if ($vic("select.picker_entity_select").length != 0 && $vic("select.picker_entity_select").attr('name').indexOf('appventus_victoirecorebundle_widgetlistingtype[items][__name__][entity]') !== -1) {
+        $vic("select.picker_entity_select").remove();
     }
     var form = $vic(this).parents('.vic-modal-content').find('.vic-tab-pane.vic-active form').filter(":visible");
+    $vic(form).trigger("victoire_widget_form_update_presubmit");
 
     loading(true);
     $vic.ajax({
@@ -68,7 +72,7 @@ $vic(document).on('click', '.vic-widget-modal a[data-modal="update"]', function(
         data: form.serialize(),
     }).done(function(response){
         if (true === response.success) {
-            $vic("#"+response.widgetId).replaceWith(response.html);
+            $vic("#"+response.widgetId).html(response.html);
             closeModal();
             loading(false);
             congrat(response.message, 10000);
@@ -86,6 +90,7 @@ $vic(document).on('click', '.vic-widget-modal a[data-modal="update"]', function(
         console.log(response);
         error('Oups, une erreur est apparue', 10000);
     });
+    $vic(form).trigger("victoire_widget_form_update_postsubmit");
 });
 
 // Delete a widget after submit
@@ -93,6 +98,7 @@ $vic(document).on('click', '.vic-widget-modal a[data-modal="delete"]', function(
     //Check that there isn't a data-toggle="vic-confirm" on it !
     if ($vic(event.target).data('toggle') != "vic-confirm" || $vic(event.target).hasClass('vic-confirmed')) {
         event.preventDefault();
+        $vic(document).trigger("victoire_widget_delete_presubmit");
 
         loading(true);
         $vic.ajax({
@@ -104,10 +110,13 @@ $vic(document).on('click', '.vic-widget-modal a[data-modal="delete"]', function(
                 var widgetContainerSelector = 'vic-widget-' + response.widgetId + '-container';
                 var widgetDiv = $vic("#" + widgetContainerSelector);
                 var widgetSlot = $vic(widgetDiv).parents('.vic-slot');
+                var slotFunction = "updateSlotActions" + $vic(widgetSlot).data('name');
+
                 //remove the div
                 widgetDiv.remove();
+
                 //close the modal
-                eval("updateSlotActions" + $vic(widgetSlot).data('name') + "()" );
+                eval(slotFunction + "()");
                 closeModal();
                 loading(false);
             } else {
@@ -116,6 +125,7 @@ $vic(document).on('click', '.vic-widget-modal a[data-modal="delete"]', function(
                 console.log(response.message);
             }
         });
+        $vic(document).trigger("victoire_widget_delete_postsubmit");
     };
 });
 
