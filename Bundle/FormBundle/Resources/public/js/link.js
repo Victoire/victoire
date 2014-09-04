e144190b5f8c6e5d641518544cc8d0434b264017
Paul Andrieux <paul@appventus.com>
2014-09-04 15:42:41 +0200

    link.js supports multiple selects

--- Bundle/FormBundle/Resources/public/js/link.js
+++ Bundle/FormBundle/Resources/public/js/link.js
@@ -1,12 +1,14 @@
-function showSelectedLinkType(select) {
-    var element = $vic(select).val();
-    var id = $vic(select).attr('id');
-    var idPrefix = id.replace('linkType', '');
-    var elements = ['page', 'url', 'route', 'attachedWidget'];
+function showSelectedLinkType(selects) {
+    $vic(selects).each(function(select) {
+        var element = $vic(this).val();
+        var id = $vic(this).attr('id');
+        var idPrefix = id.replace('linkType', '');
+        var elements = ['page', 'url', 'route', 'attachedWidget'];
 
-    for (var i = elements.length - 1; i >= 0; i--) {
-        $vic('#' + idPrefix + elements[i]).parents('.' + elements[i] + '-type').addClass('vic-hidden');
-    };
-    $vic('#' + idPrefix + element).parents('.' + element + '-type').removeClass('vic-hidden');
+        for (var i = elements.length - 1; i >= 0; i--) {
+            $vic('#' + idPrefix + elements[i]).parents('.' + elements[i] + '-type').addClass('vic-hidden');
+        };
+        $vic('#' + idPrefix + element).parents('.' + element + '-type').removeClass('vic-hidden');
+    });
 }
 
