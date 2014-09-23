d18c47187506f859a4b0c4292dcfde3ad6b84df3
Paul Andrieux <paul@appventus.com>
2014-09-23 11:38:19 +0200

    add showInFront field + form extension

---
+++ Bundle/BusinessEntityBundle/Form/Extension/BusinessEntityTypeExtension.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Victoire\Bundle\BusinessEntityBundle\Form\Extension;
+
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\FormBuilderInterface;
+use Victoire\Bundle\BusinessEntityBundle\Helper\BusinessEntityHelper;
+
+class BusinessEntityTypeExtension extends AbstractTypeExtension
+{
+    private $businessEntityHelper;
+
+    public function __construct(BusinessEntityHelper $businessEntityHelper)
+    {
+        $this->businessEntityHelper = $businessEntityHelper;
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        if (!empty($options['data_class'])
+            && $this->businessEntityHelper->findByEntityClassname($options['data_class'])) {
+
+            $builder
+            ->add(
+                'visibleOnFront',
+                'choice',
+                array(
+                    'choices' => array(
+                        1 => 'businessEntity.form.visibleOnFront.yes',
+                        0 => 'businessEntity.form.visibleOnFront.no',
+                    ),
+                    'label' => "businessEntity.form.visibleOnFront.label",
+                    'translation_domain' => 'victoire'
+                )
+            );
+
+        }
+    }
+
+    public function getExtendedType()
+    {
+        return 'form';
+    }
+}
