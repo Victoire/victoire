46d8341eaf3084f7290fb4ef8dd0af7925029d8d
lenybernard <leny@appventus.com>
2014-11-26 17:56:17 +0100

    New Tests base architecture

---
+++ Tests/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+
+use Doctrine\Common\Annotations\AnnotationRegistry;
+
+if (!file_exists($file = __DIR__.'/../vendor/autoload.php')) {
+    throw new \RuntimeException('Install the dependencies to run the test suite.');
+}
+
+$loader = require $file;
+AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
