402790f6d2d9e0cef7901e4f9889114797a0b14b
lenybernard <leny@appventus.com>
2014-11-27 22:44:53 +0100

    Add Filter bundle

---
+++ Bundle/FilterBundle/DependencyInjection/Compiler/FilterCompilerPass.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Victoire\Bundle\FilterBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\Reference;
+
+/**
+ *
+ */
+class FilterCompilerPass implements CompilerPassInterface
+{
+    /**
+     * Process filter
+     *
+     * @param ContainerBuilder $container
+     */
+    public function process(ContainerBuilder $container)
+    {
+        if ($container->hasDefinition('victoire_core.filter_chain')) {
+
+            $definition = $container->getDefinition(
+                'victoire_core.filter_chain'
+            );
+
+            $taggedServices = $container->findTaggedServiceIds(
+                'victoire_core.filter'
+            );
+
+            foreach ($taggedServices as $id => $attributes) {
+                $definition->addMethodCall(
+                    'addFilter',
+                    array(new Reference($id))
+                );
+            }
+        }
+    }
+}
