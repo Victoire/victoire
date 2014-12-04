fbde3e89f7c90a6d1bd2b4868bf69a80c0f39ebd
florian raux <florian@appventus.com>
2014-12-04 17:42:01 +0100

    fix bug on locale page

--- Bundle/PageBundle/Entity/BasePage.php
+++ Bundle/PageBundle/Entity/BasePage.php
@@ -10,7 +10,7 @@ use Victoire\Bundle\CoreBundle\Entity\View;
  *
  * @ORM\Entity(repositoryClass="Victoire\Bundle\PageBundle\Repository\BasePageRepository")
  * @ORM\Table("vic_base_page")
- * @UniqueEntity("url")
+ * @UniqueEntity(fields={"url", "locale"})
  * @ORM\HasLifecycleCallbacks
  */
 abstract class BasePage extends View
