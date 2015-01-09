87fedf031f83cbc012c80fc2193b4f0c039b5367
lenybernard <leny@appventus.com>
2015-01-09 21:59:28 +0100

    Fix error page deep search

--- Bundle/TwigBundle/Repository/ErrorPageRepository.php
+++ Bundle/TwigBundle/Repository/ErrorPageRepository.php
@@ -11,11 +11,12 @@ class ErrorPageRepository extends BasePageRepository
 {
 
     /**
-     * Get the the page that is a homepage and a published one
+     * Get a page according to the given code
+     * @param integer $code The error code
      *
      * @return Page
      */
-    public function findOneByCode($code)
+    public function findOneByCode($code, $deepMode = false)
     {
        //the query builder
         $page = $this->createQueryBuilder('page')
@@ -26,6 +27,13 @@ class ErrorPageRepository extends BasePageRepository
             ->setMaxResults(1)
             ->getQuery()
             ->getOneOrNullResult();
+
+        if (!$page && $deepMode) {
+
+            // Check for a same family error
+            // for example, for a 404 code, if the 404 error page doesn't exist, we check for a 400 errorPage
+            $page = $this->findOneByCode(floor($code/100)*100);
+        }
 
         return $page;
     }
