56af249f1e06e1177de01e91a68bec48b06b6013
lenybernard <leny@appventus.com>
2015-02-27 00:27:59 +0100

    trackAction to track BrowseEvent (visits)

---
+++ Bundle/AnalyticsBundle/EventSubscriber/BrowseEventSubscriber.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Victoire\Bundle\AnalyticsBundle\EventSubscriber;
+
+use Doctrine\Common\EventSubscriber;
+use Doctrine\ORM\Event\LoadClassMetadataEventArgs;
+use Doctrine\ORM\Mapping\Builder\ClassMetadataBuilder;
+use Victoire\Bundle\PageBundle\Entity\Page;
+
+/**
+ * This class listen Browse Event Entity changes.
+ */
+class BrowseEventSubscriber implements EventSubscriber
+{
+    protected $router;
+    protected $userClass;
+
+    /**
+     * Constructor
+     * @param string $userClass %victoire_core.user_class%
+     */
+    public function __construct($userClass)
+    {
+        $this->userClass = $userClass;
+    }
+
+    /**
+     * bind to LoadClassMetadata method
+     *
+     * @return array The subscribed events
+     */
+    public function getSubscribedEvents()
+    {
+        return array(
+            'loadClassMetadata',
+        );
+    }
+
+    /**
+     * Insert enabled widgets in base widget DiscriminatorMap
+     *
+     * @param LoadClassMetadataEventArgs $eventArgs
+     */
+    public function loadClassMetadata(LoadClassMetadataEventArgs $eventArgs)
+    {
+        $metadatas = $eventArgs->getClassMetadata();
+
+        //set a relation between Page and User to define the page author
+        $metaBuilder = new ClassMetadataBuilder($metadatas);
+
+        //Add author relation on BrowseEvent
+        if ($this->userClass && $metadatas->name === 'Victoire\Bundle\AnalyticsBundle\Entity\BrowseEvent') {
+            $metadatas->mapManyToOne(array(
+                'fieldName'    => 'author',
+                'targetEntity' => $this->userClass,
+                'cascade'      => array('persist'),
+                'joinColumns' => array(
+                    array(
+                        'name' => 'author_id',
+                        'referencedColumnName' => 'id',
+                        'onDelete' => 'SET NULL',
+                    ),
+                ),
+            ));
+        }
+    }
+}
