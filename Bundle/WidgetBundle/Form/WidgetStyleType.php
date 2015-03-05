824693be7aad5ebb0ba2cd87246b92eb22156bc4
Leny Bernard <leny@appventus.com>
2015-03-05 01:04:26 +0100

    Hey hello widget themes ! Now clients are able to choose them properly from the style form

--- Bundle/WidgetBundle/Form/WidgetStyleType.php
+++ Bundle/WidgetBundle/Form/WidgetStyleType.php
@@ -1,16 +1,29 @@
 <?php
 namespace Victoire\Bundle\WidgetBundle\Form;
 
+use Symfony\Component\Finder\Finder;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 use Victoire\Bundle\WidgetBundle\Entity\Widget;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\FormEvent;
 
 /**
  * Widget Style form type
  */
 class WidgetStyleType extends AbstractType
 {
+    private $kernel;
+    private $fileLocator;
+
+    public function __construct(Kernel $kernel, FileLocator $fileLocator)
+    {
+        $this->kernel = $kernel;
+        $this->fileLocator = $fileLocator;
+    }
     /**
      * Define form fields
      *
@@ -80,6 +93,33 @@ class WidgetStyleType extends AbstractType
             'vic_help_block' => 'widget_layout.form.containerBackground.help_block',
             'required' => false,
         ));
+
+        // add theme field
+        $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+            $data = $event->getData();
+            $form = $event->getForm();
+            //guess the bundle name
+            $widgetBundle = self::getBundleNameFromEntity(get_class($data), $this->kernel->getBundles());
+            //search for available theme files
+            $finder = Finder::create()
+                ->files()
+                ->name('*.html.twig')
+                ->in($this->fileLocator->locate('@'.$widgetBundle.'/Resources/views'))
+                ->sortByName();
+            //add the default choice
+            $choices = array(
+                '' => 'victoire.theme.default.label',
+            );
+            //prepare choices by adding in each theme
+            foreach ($finder as $key => $file) {
+                $theme = $file->getRelativePathname();
+                $theme = preg_replace('/show|\.html\.twig/', '', $theme);
+                $choices[$theme] = 'victoire.'.$widgetBundle.'.theme.'.$theme.'.label';
+            }
+            $form->add('theme', 'choice', array(
+                'choices' => $choices,
+            ));
+        });
     }
 
     /**
@@ -103,5 +143,22 @@ class WidgetStyleType extends AbstractType
     public function getName()
     {
         return 'victoire_widget_style_type';
+    }
+
+    /**
+     * Get the bundle name from an Entity namespace
+     *
+     * @return string
+     * @author lenybernard
+     **/
+    protected static function getBundleNameFromEntity($entityNamespace, $bundles)
+    {
+        $dataBaseNamespace = substr($entityNamespace, 0, strpos($entityNamespace, '\\Entity\\'));
+        foreach ($bundles as $type => $bundle) {
+            $bundleRefClass = new \ReflectionClass($bundle);
+            if ($bundleRefClass->getNamespaceName() === $dataBaseNamespace) {
+                return $type;
+            }
+        }
     }
 }
