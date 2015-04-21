98885276b042ce82ff59dc29fef4ace6d5c1edfb
Paul Andrieux <paul@appventus.com>
2015-04-21 15:56:21 +0200

    add phpunit test for widget generator

---
+++ Bundle/WidgetBundle/Tests/Generator/WidgetGeneratorTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Bundle\WidgetBundle\Tests\Generator;
+use Sensio\Bundle\GeneratorBundle\Generator\BundleGenerator;
+use Sensio\Bundle\GeneratorBundle\Model\Bundle;
+use Sensio\Bundle\GeneratorBundle\Tests\Generator\GeneratorTest;
+use Victoire\Bundle\WidgetBundle\Generator\WidgetGenerator;
+
+
+class WidgetGeneratorTest extends GeneratorTest
+{
+    public function testGenerate()
+    {
+
+        $this->getGenerator()->generate(
+            $namespace = 'Victoire\\Widget\\TestBundle',
+            $bundle = 'VictoireWidgetTestBundle',
+            $dir = $this->tmpDir,
+            $format = 'annotation',
+            $structure = null,
+            $fields = array(
+                'test' => array(
+                    'columnName' => 'test',
+                    'fieldName' => 'test',
+                    'type' => 'string',
+                    'length' => 255,
+                )
+            ),
+            $parent = 'Anakin',
+            $packagistParentName = 'friendsofvictoire/anakin-widget',
+            $contentResolver = false,
+            $parentContentResolver = false,
+            $orgname = 'friendsofvictoire'
+        );
+        $files = array(
+            'README.md',
+        );
+        foreach ($files as $file) {
+            $this->assertTrue(file_exists($this->tmpDir.'/Victoire/Widget/TestBundle/'.$file), sprintf('%s has been generated', $file));
+        }
+    }
+    protected function getGenerator()
+    {
+        $generator = new WidgetGenerator($this->filesystem);
+        $generator->setSkeletonDirs(array(
+            __DIR__.'/../../../../vendor/sensio/generator-bundle/Sensio/Bundle/GeneratorBundle/Resources/skeleton',
+            __DIR__.'/../../Resources/skeleton',
+        ));
+
+        $generator->setTemplating(new \Twig_Environment(new \Twig_Loader_Filesystem($this->skeletonDirs), array(
+            'debug'            => true,
+            'cache'            => false,
+            'strict_variables' => true,
+            'autoescape'       => false,
+        )));
+
+        return $generator;
+    }
+}
