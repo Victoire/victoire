fbb94f321cc76e0d4dcd758f41fe2d1c60ef50e0
Leny Bernard <leny@appventus.com>
2015-05-07 17:23:53 +0200

    Add Chained Repository Trait

---
+++ Bundle/CoreBundle/Repository/ChainedRepositoryTrait.php
@@ -0,0 +1,77 @@
+<?php
+namespace Victoire\Bundle\CoreBundle\Repository;
+
+use Doctrine\ORM\Query;
+use Doctrine\ORM\QueryBuilder;
+
+/**
+ * Chained Entity Repository trait
+ *
+ * This trait was written to offer repos a chained workflow.
+ */
+trait ChainedRepositoryTrait
+{
+    private $qb;
+    private $mainAlias;
+
+    /**
+     * Get query builder instance
+     * @param string $alias The entity alias
+     *
+     * @return QueryBuilder The active or default query builder
+     */
+    public function getInstance($alias = null)
+    {
+        if (!$alias && !$this->mainAlias) {
+            $namespace= explode("\\", $this->_entityName);
+            $alias = strtolower(end($namespace));
+            $this->mainAlias = $alias;
+        }elseif ($alias){
+            $this->mainAlias = $alias;
+        }
+
+        return $this->qb ? $this->qb : $this->qb = $this->createQueryBuilder($this->mainAlias);
+    }
+
+    /**
+     * Set query builder instance
+     * @param QueryBuilder $qb The queryBuilder
+     *
+     * @return BaseEntityRepository This repository
+     */
+    public function setInstance(QueryBuilder $qb)
+    {
+        $this->qb = $qb;
+
+        return $this;
+    }
+
+    /**
+     * Clears the current QueryBuilder instance
+     * @return BaseEntityRepository This repository
+     */
+    public function clearInstance()
+    {
+        $this->qb = null;
+
+        return $this;
+    }
+
+    /**
+     * Run active query
+     * @param method        $method        The method to run
+     * @param hydrationMode $hydrationMode How the results will be (Object ? Array )
+     * @param boolean       $autoClear     AutoClear means reset active instance
+     *
+     * @return array()
+     */
+    public function run($method = 'getResult', $hydrationMode = Query::HYDRATE_OBJECT, $autoClear = true)
+    {
+        $results = $this->qb->getQuery()->$method($hydrationMode);
+        if ($autoClear) {
+            $this->clearInstance();
+        }
+
+        return $results;
+    }
+}
