61998f1c538916b3911d9d3331a2876a4acea3fc
Leny Bernard <leny@appventus.com>
2015-05-09 09:43:53 +0200

    fix typo

--- Tests/Functionnal/src/Acme/AppBundle/DependencyInjection/Configuration.php
+++ Tests/Functionnal/src/Acme/AppBundle/DependencyInjection/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Victoire\Widget\AnakinBundle\DependencyInjection;
+namespace Acme\AppBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
