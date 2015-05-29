9d0cc013a05f168bdd87105bf0d1e46df9db26de
Charlie <charlie@appventus.com>
2015-05-29 09:41:16 +0200

    moved logic from basepagetype to viewtype

--- Bundle/PageBundle/Repository/BasePageRepository.php
+++ Bundle/PageBundle/Repository/BasePageRepository.php
@@ -3,6 +3,7 @@ namespace Victoire\Bundle\PageBundle\Repository;
 
 use Doctrine\ORM\Query;
 use Victoire\Bundle\PageBundle\Entity\PageStatus;
+use Victoire\Bundle\CoreBundle\Repository\ViewRepository;
 
 /**
  * The Page repository
