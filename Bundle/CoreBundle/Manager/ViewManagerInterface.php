54984e170b7d00294049aa558d577b31feffbc20
Charlie <charlie@appventus.com>
2015-08-18 17:55:26 +0200

    add php doc

--- Bundle/CoreBundle/Manager/ViewManagerInterface.php
+++ Bundle/CoreBundle/Manager/ViewManagerInterface.php
@@ -4,11 +4,19 @@ namespace Victoire\Bundle\CoreBundle\Manager;
 
 use Victoire\Bundle\CoreBundle\Entity\View;
 
+/**
+ * List view managers methods
+ *
+ * Interface ViewManagerInterface
+ * @package Victoire\Bundle\CoreBundle\Manager
+ */
 interface ViewManagerInterface
 {
     /**
-     * Build reference for a view
+     * Build Reference for a view
      *
+     * @param View $view
+     * @return ViewReference
      */
     public function buildReference(View $view);
 }
