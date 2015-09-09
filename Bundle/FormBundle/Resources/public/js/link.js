ffe189b625234381684536dde040023e29b2293c
Vincent Chapron <vincent@chapron.io>
2015-09-09 18:13:10 +0200

    Update showSelectedLinkType option to update col size with the number of fields and detect the refresh on change with the hidden input and show the good fields.

--- Bundle/FormBundle/Resources/public/js/link.js
+++ Bundle/FormBundle/Resources/public/js/link.js
@@ -1,15 +1,29 @@
+/**
+ * This function is use to display or hide link fields.
+ * If the hidden input "current" is not null, this function show the good fields
+ * This hidden input has been added in the linkType because when the "data-refreshOnChange" was used,
+ * link field took 'none link' value, or value shouldn't be change.
+ */
 function showSelectedLinkType(selects) {
-    $vic(selects).each(function(select) {
-        var element = $vic(this).val();
-        var id = $vic(this).attr('id');
-        var idPrefix = id.replace('linkType', '');
-        var elements = ['viewReference', 'url', 'route', 'attachedWidget', 'target'];
+    $vic(selects).each(function(){
+        var select    = $vic(this);
+        var current   = $vic('.vic-current-link').val();
+        (current) ? $vic('.vic-current-link').val('') : current = $vic(this).val();
+        var container = select.closest('.vic-link-container');
+        var active    = container.find('.' + current + '-type');
+        var count     = (current != 'none' && container.find('.analytics-type').length) ? 2 : 1;
+        var length    = (active.length + count < 4) ? 12 / (active.length + count) : 6;
+        var column    = container.hasClass('-horizontal') ?'col-sm-' + length : 'col-sm-12';
 
-        for (var i = elements.length - 1; i >= 0; i--) {
-            $vic('#' + idPrefix + elements[i]).parents('.' + elements[i] + '-type').addClass('vic-hidden');
-        };
-        console.log('#' + idPrefix + element);
-        $vic('#' + idPrefix + element).parents('.' + element + '-type').removeClass('vic-hidden');
+        container.find('.vic-form-group').addClass('vic-hidden');
+        for (var i = 0; i <= 12; i++) {
+            container.find('.vic-form-group').removeClass('col-sm-' + i);
+        }
+        select.closest('.vic-form-group').removeClass('vic-hidden').addClass(column);
+        active.removeClass('vic-hidden').addClass(column);
+        if (count > 1) {
+            $('.analytics-type').removeClass('vic-hidden').addClass(column);
+        }
     });
 }
 
