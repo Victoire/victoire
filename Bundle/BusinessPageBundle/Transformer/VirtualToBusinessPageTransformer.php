d64fcc0dc5d0b823aeddeba39001636f2e8a6592
Paul Andrieux <paul@appventus.com>
2015-09-13 21:29:30 +0200

    re-enable static url

---
+++ Bundle/BusinessPageBundle/Transformer/VirtualToBusinessPageTransformer.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Victoire\Bundle\BusinessPageBundle\Transformer;
+
+use Victoire\Bundle\BusinessPageBundle\Entity\BusinessPage;
+use Victoire\Bundle\BusinessPageBundle\Entity\VirtualBusinessPage;
+
+class VirtualToBusinessPageTransformer
+{
+
+    public function transform(VirtualBusinessPage &$sourceObject)
+    {
+        $bp = new BusinessPage();
+
+        $sourceReflection = new \ReflectionObject($sourceObject);
+        $sourceProperties = $sourceReflection->getProperties();
+        foreach ($sourceProperties as $sourceProperty) {
+            $sourceProperty->setAccessible(true);
+            $name = $sourceProperty->getName();
+
+            $value = $sourceProperty->getValue($sourceObject);
+            $method = 'set'.ucfirst($name);
+            if (method_exists($bp, $method) && $value !== null) {
+                $bp->$method($value);
+            }
+        }
+
+        $sourceObject = $bp;
+    }
+
+}
