d2ee3671657a28a5f0d3a4ce12d63ba6e52df644
Paul Andrieux <paul@appventus.com>
2015-09-17 20:01:42 +0200

    the EntityProxyProvider fetch DB to give existing EP or create a new one

---
+++ Bundle/BusinessEntityBundle/Provider/EntityProxyProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Victoire\Bundle\BusinessEntityBundle\Provider;
+
+use Doctrine\ORM\EntityManager;
+use Victoire\Bundle\BusinessEntityBundle\Entity\BusinessEntity;
+use Victoire\Bundle\CoreBundle\Entity\EntityProxy;
+
+class EntityProxyProvider {
+
+    public function getEntityProxy($entity, BusinessEntity $businessEntity, EntityManager $em)
+    {
+
+        $entityProxy = $em->getRepository('Victoire\Bundle\CoreBundle\Entity\EntityProxy')->findOneBy([$businessEntity->getId() => $entity ]);
+        if (!$entityProxy) {
+            $entityProxy = new EntityProxy();
+            $entityProxy->setEntity($entity, $businessEntity->getName());
+        }
+
+        return $entityProxy;
+    }
+} 
