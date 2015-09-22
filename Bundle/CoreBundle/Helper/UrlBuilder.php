aff153e6682a4425046f68cb756c5817d11e9463
Paul Andrieux <paul@appventus.com>
2015-09-22 17:53:36 +0200

    if homepage, do not insert an empty array value cause it will generate a double /

--- Bundle/CoreBundle/Helper/UrlBuilder.php
+++ Bundle/CoreBundle/Helper/UrlBuilder.php
@@ -1,30 +1,34 @@
 <?php
 namespace Victoire\Bundle\CoreBundle\Helper;
 
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\UnitOfWork;
+use Symfony\Bundle\FrameworkBundle\Routing\Router;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Victoire\Bundle\CoreBundle\Entity\Route;
 use Victoire\Bundle\CoreBundle\Entity\WebViewInterface;
+use Victoire\Bundle\PageBundle\Repository\PageRepository;
 
 /**
  * ref: victoire_core.url_builder
  */
 class UrlBuilder
 {
-
-
-
+    
     /**
      * Builds the page's url by get all page parents slugs and implode them with "/".
      * @param WebViewInterface $view
+     * @param integer $depth
      *
      * @return string $url
      */
     public function buildUrl(WebViewInterface $view)
     {
+        $slug = [];
         // build url binded with parents url
-        if (method_exists($view, 'isHomepage') && $view->isHomepage()) {
-            $slug = array('');
-        } else if (method_exists($view, 'getStaticUrl') && $view->getStaticUrl() != null && $view->getStaticUrl() != '') {
+        if (method_exists($view, 'getStaticUrl') && $view->getStaticUrl() != null && $view->getStaticUrl() != '' ) {
             $slug = array($view->getStaticUrl());
-        } else {
+        } else if (!(method_exists($view, 'isHomepage') && $view->isHomepage())) {
             $slug = array($view->getSlug());
         }
 
@@ -44,7 +48,7 @@ class UrlBuilder
     /**
      * Get the array of slugs of the parents
      * @param WebViewInterface $view
-     * @param string[]            $slugs
+     * @param array            $slugs
      *
      * @return array
      */
@@ -53,14 +57,12 @@ class UrlBuilder
         $parent = $view->getParent();
 
         if ($parent !== null) {
-            if (method_exists($parent, 'isHomepage') && $parent->isHomepage()) {
-                $slug = '';
-            } else if (method_exists($parent, 'getStaticUrl') && $parent->getStaticUrl() != null && $parent->getStaticUrl() != '') {
-                $slug = $parent->getStaticUrl();
-            } else {
-                $slug = $parent->getSlug();
+            if (method_exists($parent, 'getStaticUrl') && $parent->getStaticUrl() != null && $parent->getStaticUrl() != '' ) {
+                array_push($slugs, $parent->getStaticUrl());
+            } else if (!(method_exists($parent, 'isHomepage') && $parent->isHomepage())) {
+                array_push($slugs, $parent->getSlug());
             }
-            array_push($slugs, $slug);
+
             if ($parent->getParent() !== null) {
                 $slugs = array_merge($slugs, $this->getParentSlugs($parent, $slugs));
             }
