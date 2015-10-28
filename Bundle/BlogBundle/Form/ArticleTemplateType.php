1da867b932f8c9409e9830a3c0e1c3450508f43b
Vincent Chapron <vincent@chapron.io>
2015-10-28 10:03:08 +0100

    Add article template fields, hidden if only 1 result or select if more

--- Bundle/BlogBundle/Form/ArticleTemplateType.php
+++ Bundle/BlogBundle/Form/ArticleTemplateType.php
@@ -36,6 +36,28 @@ class ArticleTemplateType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder->add('pattern')->remove('visibleOnFront');
+
+        $builder->get('pattern')->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+            $articleTemplateRepo = $this->entityManager->getRepository('VictoireBlogBundle:ArticleTemplate');
+
+            $parent = $event->getForm()->getParent()->getParent();
+            $blog_id = $parent->getData()->getBlog()->getId();
+            if ($articleTemplateRepo->filterByBlog($blog_id)->getCount('parent')->run('getSingleScalarResult') > 1) {
+                $articlePatterns = function (EntityRepository $repo) use ($blog_id) {
+                    return $repo->filterByBlog($blog_id)->getInstance();
+                };
+                $parent->add('pattern', null, [
+                    'label'         => 'form.view.type.pattern.label',
+                    'property'      => 'name',
+                    'required'      => true,
+                    'query_builder' => $articlePatterns,
+                ]);
+            } else {
+                $parent->add('pattern', 'hidden', [
+                    'data' => $articleTemplateRepo->filterByBlog($blog_id)->run('getSingleResult')->getId(),
+                ]);
+            }
+        });
     }
 
     /**
