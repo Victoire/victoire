859c7027b476b19af64a350e9b1270122a42441d
Leny BERNARD <leny@appventus.com>
2015-11-13 10:55:17 +0100

    Split ViewHelper in two services (ViewTranslationManager)

--- Bundle/CoreBundle/Helper/ViewHelper.php
+++ Bundle/CoreBundle/Helper/ViewHelper.php
@@ -19,201 +19,48 @@ use Victoire\Bundle\ViewReferenceBundle\Provider\ViewReferenceProvider;
  */
 class ViewHelper
 {
-    protected $parameterConverter;
-    protected $businessEntityHelper;
-    protected $em;
-    protected $viewReferenceBuilder;
-    protected $viewReferenceHelper;
-    protected $viewReferenceProvider;
+    /**
+     * @var EntityManager
+     */
+    private $entityManager;
+    /**
+     * @var ViewReferenceProvider
+     */
+    private $viewReferenceProvider;
+    /**
+     * @var ViewReferenceHelper
+     */
+    private $viewReferenceHelper;
 
     /**
      * Constructor.
      *
-     * @param BETParameterConverter $parameterConverter
-     * @param BusinessEntityHelper  $businessEntityHelper
-     * @param EntityManager         $entityManager
-     * @param ViewReferenceBuilder  $viewReferenceBuilder
-     * @param ViewReferenceHelper   $viewReferenceHelper
-     * @param ViewReferenceProvider $viewReferenceProvider
-     *
      * @internal param $ViewManagerChain $$viewManagerChain
+     * @param EntityManager $entityManager
+     * @param ViewReferenceHelper $viewReferenceHelper
+     * @param ViewReferenceProvider $viewReferenceProvider
      */
     public function __construct(
-        BETParameterConverter $parameterConverter,
-        BusinessEntityHelper $businessEntityHelper,
         EntityManager $entityManager,
-        ViewReferenceBuilder $viewReferenceBuilder,
-        ViewReferenceHelper $viewReferenceHelper,
-        ViewReferenceProvider $viewReferenceProvider
+        ViewReferenceProvider $viewReferenceProvider,
+        ViewReferenceHelper $viewReferenceHelper
     ) {
-        $this->parameterConverter = $parameterConverter;
-        $this->businessEntityHelper = $businessEntityHelper;
-        $this->em = $entityManager;
-        $this->viewReferenceBuilder = $viewReferenceBuilder;
-        $this->viewReferenceHelper = $viewReferenceHelper;
-        $this->viewReferenceProvider = $viewReferenceProvider;
-    }
 
-    //@todo Make it dynamic please
-    protected $pageParameters = [
-        'name',
-        'bodyId',
-        'bodyClass',
-        'slug',
-        'url',
-        'locale',
-    ];
+        $this->entityManager = $entityManager;
+        $this->viewReferenceProvider = $viewReferenceProvider;
+        $this->viewReferenceHelper = $viewReferenceHelper;
+    }
 
     /**
      * @return WebViewInterface[]
      */
     public function buildViewsReferences()
     {
-        $viewsHierarchy = $this->em->getRepository('VictoireCoreBundle:View')->getRootNodes();
-        $views = $this->viewReferenceProvider->getReferencableViews($viewsHierarchy, $this->em);
+        $viewsHierarchy = $this->entityManager->getRepository('VictoireCoreBundle:View')->getRootNodes();
+        $views = $this->viewReferenceProvider->getReferencableViews($viewsHierarchy, $this->entityManager);
 
-        $this->buildViewReferenceRecursively($views);
+        $this->viewReferenceHelper->buildViewReferenceRecursively($views, $this->entityManager);
 
         return $views;
-    }
-
-    /**
-     * @param [] $tree
-     */
-    public function buildViewReferenceRecursively($tree) {
-        foreach ($tree as $branch) {
-            /** @var WebViewInterface $view */
-            $view = $branch['view'];
-            $view->setViewReference($this->viewReferenceBuilder->buildViewReference($view, $this->em));
-            if (!empty($branch['children'])) {
-                /** @var WebViewInterface $children */
-                $children = $branch['children'];
-                $this->buildViewReferenceRecursively($children);
-            }
-        }
-    }
-
-    /**
-     * @param View $view, the view to translatate
-     * @param $templatename the new name of the view
-     * @param $loopindex the current loop of iteration in recursion
-     * @param $locale the target locale to translate view
-     *
-     * this methods allow you to add a translation to any view
-     * recursively to its subview
-     */
-    public function addTranslation(View $view, $viewName = null, $locale)
-    {
-        $template = null;
-        if ($view->getTemplate()) {
-            $template = $view->getTemplate();
-            if ($template->getI18n()->getTranslation($locale)) {
-                $template = $template->getI18n()->getTranslation($locale);
-            } else {
-                $templateName = $template->getName().'-'.$locale;
-                $this->em->refresh($view);
-                $template = $this->addTranslation($template, $templateName, $locale);
-            }
-        }
-        $view->setLocale($locale);
-        $view->setTemplate($template);
-        $clonedView = $this->cloneView($view, $viewName, $locale);
-        if ($clonedView instanceof BasePage && $view->getTemplate()) {
-            $template->addPage($clonedView);
-        }
-        $i18n = $view->getI18n();
-        $i18n->setTranslation($locale, $clonedView);
-        $this->em->persist($clonedView);
-        $this->em->refresh($view);
-        $this->em->flush();
-
-        return $clonedView;
-    }
-
-    /**
-     * @param View $view
-     * @param $etmplateName the future name of the clone
-     *
-     * this methods allows you to clone a view and its widgets and also the widgetmap
-     */
-    public function cloneView(View $view, $templateName = null)
-    {
-        $clonedView = clone $view;
-        $this->em->refresh($view);
-        $widgetMapClone = $clonedView->getWidgetMap(false);
-        $arrayMapOfWidgetMap = [];
-        if (null !== $templateName) {
-            $clonedView->setName($templateName);
-        }
-
-        $clonedView->setId(null);
-        $this->em->persist($clonedView);
-
-        if ($clonedView instanceof BusinessTemplate) {
-            $clonedView = $this->cloneBusinessTemplate($clonedView);
-        } else {
-            $widgetLayoutSlots = [];
-            $newWidgets = [];
-            foreach ($clonedView->getWidgets() as $widgetKey => $widgetVal) {
-                $clonedWidget = clone $widgetVal;
-                $clonedWidget->setId(null);
-                $clonedWidget->setView($clonedView);
-                $this->em->persist($clonedWidget);
-                $newWidgets[] = $clonedWidget;
-                $arrayMapOfWidgetMap[$widgetVal->getId()] = $clonedWidget;
-                if ($widgetVal instanceof WidgetLayout) {
-                    $id = $widgetVal->getId();
-                    $widgetLayoutSlots[$id] = $clonedWidget;
-                }
-            }
-            $clonedView->setWidgets($newWidgets);
-            $this->em->persist($clonedView);
-            $this->em->flush();
-            $widgetSlotMap = [];
-            foreach ($widgetLayoutSlots as $_id => $_widget) {
-                foreach ($clonedView->getWidgets() as $_clonedWidget) {
-                    if (preg_match('/^'.$_id.'_(.)/', $_clonedWidget->getSlot(), $matches)) {
-                        $newSlot = $_widget->getId().'_'.$matches[1];
-                        $oldSlot = $_clonedWidget->getSlot();
-                        $_clonedWidget->setSlot($newSlot);
-                        $widgetSlotMap[$oldSlot] = $newSlot;
-                    }
-                }
-            }
-
-            $this->em->flush();
-            foreach ($widgetMapClone as $wigetSlotCloneKey => $widgetSlotCloneVal) {
-                foreach ($widgetSlotCloneVal as $widgetMapItemKey => $widgetMapItemVal) {
-                    if (isset($arrayMapOfWidgetMap[$widgetMapItemVal['widgetId']])) {
-                        $widgetId = $arrayMapOfWidgetMap[$widgetMapItemVal['widgetId']]->getId();
-                        $widgetMapItemVal['widgetId'] = $widgetId;
-                        if (array_key_exists($wigetSlotCloneKey, $widgetSlotMap)) {
-                            $wigetSlotCloneKey = $widgetSlotMap[$wigetSlotCloneKey];
-                        }
-                        $widgetMapClone[$wigetSlotCloneKey][$widgetMapItemKey] = $widgetMapItemVal;
-                    }
-                }
-            }
-
-            $clonedView->setSlots([]);
-            $clonedView->setWidgetMap($widgetMapClone);
-            $this->em->persist($clonedView);
-            $this->em->flush();
-        }
-
-        return $clonedView;
-    }
-
-    /**
-     * @param BusinessTemplate $view
-     * @param $etmplateName the future name of the clone
-     *
-     * this methods allows you to clone a BusinessTemplate
-     */
-    protected function cloneBusinessTemplate(BusinessTemplate $view)
-    {
-        $businessEntityId = $view->getBusinessEntityId();
-        $businessEntity = $this->get('victoire_core.helper.business_entity_helper')->findById($businessEntityId);
-        $businessProperties = $businessEntity->getBusinessPropertiesByType('seoable');
     }
 }
---
+++ Bundle/I18nBundle/Manager/ViewTranslationManager.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace Victoire\Bundle\I18nBundle\Manager;
+
+use Doctrine\Orm\EntityManager;
+use Victoire\Bundle\BusinessEntityBundle\Converter\ParameterConverter as BETParameterConverter;
+use Victoire\Bundle\BusinessEntityBundle\Helper\BusinessEntityHelper;
+use Victoire\Bundle\BusinessPageBundle\Entity\BusinessTemplate;
+use Victoire\Bundle\CoreBundle\Entity\View;
+use Victoire\Bundle\CoreBundle\Entity\WebViewInterface;
+use Victoire\Bundle\PageBundle\Entity\BasePage;
+use Victoire\Bundle\ViewReferenceBundle\Builder\ViewReferenceBuilder;
+use Victoire\Bundle\ViewReferenceBundle\Helper\ViewReferenceHelper;
+use Victoire\Bundle\ViewReferenceBundle\Provider\ViewReferenceProvider;
+
+/**
+ * Page helper
+ * ref: victoire_i18n.view_translation_manager.
+ */
+class ViewTranslationManager
+{
+    protected $parameterConverter;
+    protected $businessEntityHelper;
+    protected $em;
+    protected $viewReferenceBuilder;
+    protected $viewReferenceHelper;
+    protected $viewReferenceProvider;
+
+    /**
+     * Constructor.
+     *
+     * @param EntityManager         $entityManager
+     *
+     */
+    public function __construct(EntityManager $entityManager)
+    {
+        $this->em = $entityManager;
+    }
+
+    /**
+     * @param View $view, the view to translatate
+     * @param $templatename the new name of the view
+     * @param $loopindex the current loop of iteration in recursion
+     * @param $locale the target locale to translate view
+     *
+     * this methods allow you to add a translation to any view
+     * recursively to its subview
+     */
+    public function addTranslation(View $view, $viewName = null, $locale)
+    {
+        $template = null;
+        if ($view->getTemplate()) {
+            $template = $view->getTemplate();
+            if ($template->getI18n()->getTranslation($locale)) {
+                $template = $template->getI18n()->getTranslation($locale);
+            } else {
+                $templateName = $template->getName().'-'.$locale;
+                $this->em->refresh($view);
+                $template = $this->addTranslation($template, $templateName, $locale);
+            }
+        }
+        $view->setLocale($locale);
+        $view->setTemplate($template);
+        $clonedView = $this->cloneView($view, $viewName, $locale);
+        if ($clonedView instanceof BasePage && $view->getTemplate()) {
+            $template->addPage($clonedView);
+        }
+        $i18n = $view->getI18n();
+        $i18n->setTranslation($locale, $clonedView);
+        $this->em->persist($clonedView);
+        $this->em->refresh($view);
+        $this->em->flush();
+
+        return $clonedView;
+    }
+
+    /**
+     * @param View $view
+     * @param $templateName the future name of the clone
+     *
+     * this methods allows you to clone a view and its widgets and also the widgetmap
+     */
+    public function cloneView(View $view, $templateName = null)
+    {
+        $clonedView = clone $view;
+        $this->em->refresh($view);
+        $widgetMapClone = $clonedView->getWidgetMap(false);
+        $arrayMapOfWidgetMap = [];
+        if (null !== $templateName) {
+            $clonedView->setName($templateName);
+        }
+
+        $clonedView->setId(null);
+        $this->em->persist($clonedView);
+
+        if (!$clonedView instanceof BusinessTemplate) {
+            $widgetLayoutSlots = [];
+            $newWidgets = [];
+            foreach ($clonedView->getWidgets() as $widgetKey => $widgetVal) {
+                $clonedWidget = clone $widgetVal;
+                $clonedWidget->setId(null);
+                $clonedWidget->setView($clonedView);
+                $this->em->persist($clonedWidget);
+                $newWidgets[] = $clonedWidget;
+                $arrayMapOfWidgetMap[$widgetVal->getId()] = $clonedWidget;
+                if ($widgetVal instanceof WidgetLayout) {
+                    $id = $widgetVal->getId();
+                    $widgetLayoutSlots[$id] = $clonedWidget;
+                }
+            }
+            $clonedView->setWidgets($newWidgets);
+            $this->em->persist($clonedView);
+            $this->em->flush();
+            $widgetSlotMap = [];
+            foreach ($widgetLayoutSlots as $_id => $_widget) {
+                foreach ($clonedView->getWidgets() as $_clonedWidget) {
+                    if (preg_match('/^'.$_id.'_(.)/', $_clonedWidget->getSlot(), $matches)) {
+                        $newSlot = $_widget->getId().'_'.$matches[1];
+                        $oldSlot = $_clonedWidget->getSlot();
+                        $_clonedWidget->setSlot($newSlot);
+                        $widgetSlotMap[$oldSlot] = $newSlot;
+                    }
+                }
+            }
+
+            $this->em->flush();
+            foreach ($widgetMapClone as $wigetSlotCloneKey => $widgetSlotCloneVal) {
+                foreach ($widgetSlotCloneVal as $widgetMapItemKey => $widgetMapItemVal) {
+                    if (isset($arrayMapOfWidgetMap[$widgetMapItemVal['widgetId']])) {
+                        $widgetId = $arrayMapOfWidgetMap[$widgetMapItemVal['widgetId']]->getId();
+                        $widgetMapItemVal['widgetId'] = $widgetId;
+                        if (array_key_exists($wigetSlotCloneKey, $widgetSlotMap)) {
+                            $wigetSlotCloneKey = $widgetSlotMap[$wigetSlotCloneKey];
+                        }
+                        $widgetMapClone[$wigetSlotCloneKey][$widgetMapItemKey] = $widgetMapItemVal;
+                    }
+                }
+            }
+
+            $clonedView->setSlots([]);
+            $clonedView->setWidgetMap($widgetMapClone);
+            $this->em->persist($clonedView);
+            $this->em->flush();
+        }
+
+        return $clonedView;
+    }
+}
