88e9aff3e3c0cb4baac3b8faadc9865243842c58
Charlie <charlielucas.cl@gmail.com>
2016-01-07 14:51:11 +0100

    create viewReferencebundleEvents

---
+++ Bundle/ViewReferenceBundle/ViewReferenceEvents.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Victoire\Bundle\ViewReferenceBundle;
+
+
+final class ViewReferenceEvents
+{
+
+    /**
+     * The UPDATE_VIEW_REFERENCE event occurs when a redis reference need to be updated.
+     */
+    const UPDATE_VIEW_REFERENCE = 'victoire.view_reference.update';
+    
+    /**
+     * The REMOVE_VIEW_REFERENCE event occurs when the redis reference need to be deleted.
+     */
+    const REMOVE_VIEW_REFERENCE = 'victoire.view_reference.remove';
+}
