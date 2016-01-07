76af1eb87f937015550fdee8c64d2032c2d938f7
Charlie <charlielucas.cl@gmail.com>
2016-01-07 14:40:26 +0100

    create a listener to manage specific viewReference

---
+++ Bundle/ViewReferenceBundle/Listener/ViewReferenceListener.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Victoire\Bundle\ViewReferenceBundle\Listener;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Victoire\Bundle\CoreBundle\Entity\View;
+use Victoire\Bundle\ViewReferenceBundle\Builder\ViewReferenceBuilder;
+use Victoire\Bundle\ViewReferenceBundle\Cache\Redis\ViewReferenceRedisDriver;
+use Victoire\Bundle\ViewReferenceBundle\Event\ViewReferenceEvent;
+use Victoire\Bundle\ViewReferenceBundle\ViewReferenceEvents;
+
+/**
+ * Class ViewReferenceListener
+ * @package Victoire\Bundle\ViewReferenceBundle\Listener
+ *
+ * This listener is used to update or remove a reference in redis
+ */
+class ViewReferenceListener implements EventSubscriberInterface
+{
+    private $viewReferenceBuilder;
+    private $viewReferenceDriver;
+    private $em;
+
+    /**
+     * ViewReferenceListener constructor.
+     * @param ViewReferenceBuilder $viewReferenceBuilder
+     * @param ViewReferenceRedisDriver $viewReferenceDriver
+     * @param EntityManagerInterface $em
+     */
+    public function __construct(ViewReferenceBuilder $viewReferenceBuilder, ViewReferenceRedisDriver $viewReferenceDriver, EntityManagerInterface $em)
+    {
+        $this->viewReferenceBuilder = $viewReferenceBuilder;
+        $this->viewReferenceDriver = $viewReferenceDriver;
+        $this->em = $em;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function getSubscribedEvents()
+    {
+        return array(
+            ViewReferenceEvents::UPDATE_VIEW_REFERENCE => "updateViewReference",
+            ViewReferenceEvents::REMOVE_VIEW_REFERENCE => "removeViewReference",
+        );
+    }
+
+    /**
+     * This method is call when a viewReference need to be update
+     * @param ViewReferenceEvent $event
+     */
+    public function updateViewReference(ViewReferenceEvent $event)
+    {
+        $view = $event->getView();
+        $viewReference = $this->viewReferenceBuilder->buildViewReference($view, $this->em);
+        $this->viewReferenceDriver->saveReference($viewReference);
+
+    }
+    /**
+     * This method is call when a viewReference need to be remove
+     * @param ViewReferenceEvent $event
+     */
+    public function removeViewReference(ViewReferenceEvent $event)
+    {
+        $view = $event->getView();
+        $viewReference = $this->viewReferenceBuilder->buildViewReference($view, $this->em);
+        $this->viewReferenceDriver->removeReference($viewReference);
+    }
+}
