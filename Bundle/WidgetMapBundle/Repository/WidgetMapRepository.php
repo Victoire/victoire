550bb9de8aece275585a58c9576dd48cef839d8e
Paul Andrieux <paul@appventus.com>
2016-01-07 09:05:10 +0100

    move WidgetMap in WidgetMapBundle and declare it as an entity

---
+++ Bundle/WidgetMapBundle/Repository/WidgetMapRepository.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Victoire\Bundle\WidgetMapBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+use Victoire\Bundle\CoreBundle\Entity\View;
+use Victoire\Bundle\WidgetBundle\Entity\Widget;
+
+
+class WidgetMapRepository extends EntityRepository
+{
+}
