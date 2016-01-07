09e796923ed028081ed89265424f7e2b5dba0fec
Paul Andrieux <paul@appventus.com>
2016-01-07 09:02:40 +0100

    enable web debug toolbar in test env

--- Tests/Functionnal/web/app.php
+++ Tests/Functionnal/web/app.php
@@ -1,12 +1,16 @@
 <?php
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Debug\Debug;
 
 require_once __DIR__.'/../app/AppKernel.php';
-$kernel = new AppKernel('test', false);
+Debug::enable();
+$kernel = new AppKernel('test', true);
 $kernel->loadClassCache();
 Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
+
+
