8de6cfae182e42deadfce37ba7d1190a03133ef2
Leny BERNARD <leny@appventus.com>
2016-01-20 16:11:21 +0100

    remove template's parent type because we use template property for this

--- Bundle/CoreBundle/Form/ViewType.php
+++ Bundle/CoreBundle/Form/ViewType.php
@@ -69,21 +69,19 @@ abstract class ViewType extends AbstractType
                 ]);
             }
             if (!$form->has('locale') && count($choices = $this->getAvailableLocales($view)) > 1) {
+                $data = $view->getLocale() ?: $this->currentLocale;
                 $form->add('locale', 'choice', [
                         'expanded' => false,
                         'multiple' => false,
                         'choices'  => $choices,
                         'label'    => 'form.view.type.locale.label',
-                        'data'     => $this->currentLocale,
+                        'data'     => $data,
                     ]
                 );
             }
 
-            //If view is either a BEPP or a BEP and is not an Article BEP, we do not allow to choose parent because it will be set
-            if (!$view instanceof ArticleTemplate) {
-                // vérifie si l'objet Product est "nouveau"
-                // Si aucune donnée n'est passée au formulaire, la donnée est "null".
-                // Ce doit être considéré comme une nouvelle "View"
+            //If view is an Article BEP, we do not allow to choose parent because it will be set automatically
+            if (!$view instanceof ArticleTemplate && ClassUtils::getClass($view) != 'Victoire\Bundle\TemplateBundle\Entity\Template') {
                 if (!$view || null === $view->getId()) {
                     $getAllPageWithoutMe = function (EntityRepository $repo) {
                         return $repo->getAll()->getInstance();
@@ -97,22 +95,14 @@ abstract class ViewType extends AbstractType
                     };
                 }
 
-                if (ClassUtils::getClass($view) == 'Victoire\Bundle\TemplateBundle\Entity\Template') {
-                    $class = 'Victoire\Bundle\TemplateBundle\Entity\Template';
-                    $required = false;
-                } else {
-                    $class = 'Victoire\Bundle\PageBundle\Entity\BasePage';
-                    $required = true;
-                }
-
                 $form->add(
                     'parent',
                     null,
                     [
-                        'class'         => $class,
+                        'class'         => 'Victoire\Bundle\PageBundle\Entity\BasePage',
                         'label'         => 'form.view.type.parent.label',
                         'query_builder' => $getAllPageWithoutMe,
-                        'required'      => $required,
+                        'required'      => true,
                     ]
                 );
             }
