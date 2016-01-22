e2cf5ee524ec121e42895ce62896fb1518217e32
Leny BERNARD <leny@appventus.com>
2016-01-22 12:37:41 +0100

    add a isSerialized function to avoid to run serialize function on non-serialized string and log too much errors

--- Bundle/ViewReferenceBundle/Connector/Redis/ViewReferenceRedisTool.php
+++ Bundle/ViewReferenceBundle/Connector/Redis/ViewReferenceRedisTool.php
@@ -34,10 +34,12 @@ class ViewReferenceRedisTool
     public function unredislize($data)
     {
         $data = urldecode($data);
-        // unserialize data if we can
-        $unserializedData = @unserialize($data);
-        if ($unserializedData !== false) {
-            return $unserializedData;
+
+        if (self::isSerialized($data)) {
+            $unserializedData = @unserialize($data);
+            if ($unserializedData !== false) {
+                return $unserializedData;
+            }
         }
 
         return $data;
@@ -90,5 +92,31 @@ class ViewReferenceRedisTool
     public function generateKey($alias, $id)
     {
         return $alias.':'.$id;
+    }
+
+    public static function isSerialized($data) {
+        // if it isn't a string, it isn't serialized
+        if ( !is_string( $data ) )
+            return false;
+        $data = trim( $data );
+        if ( 'N;' == $data )
+            return true;
+        if ( !preg_match( '/^([adObis]):/', $data, $badions ) )
+            return false;
+        switch ( $badions[1] ) {
+            case 'a' :
+            case 'O' :
+            case 's' :
+                if ( preg_match( "/^{$badions[1]}:[0-9]+:.*[;}]\$/s", $data ) )
+                    return true;
+                break;
+            case 'b' :
+            case 'i' :
+            case 'd' :
+                if ( preg_match( "/^{$badions[1]}:[0-9.E-]+;\$/", $data ) )
+                    return true;
+                break;
+        }
+        return false;
     }
 }
