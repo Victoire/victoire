08b0cab4a371f03fd803fcec8d69467334867e1b
Paul Andrieux <paul@appventus.com>
2016-01-28 11:37:18 +0100

    create a new widget button in empty slots

--- Bundle/CoreBundle/Resources/script/ngApp/controllers/slot/SlotCtrl.js
+++ Bundle/CoreBundle/Resources/script/ngApp/controllers/slot/SlotCtrl.js
@@ -1,14 +1,18 @@
-angular.module('ngApp').controller("SlotController", ["$scope",
-    function($scope) {
+angular.module('ngApp').controller("SlotController", ["$scope", "$compile",
+    function($scope, $compile) {
         this.init = function(slotId, options) {
             $scope.slotId = slotId;
             $scope.options = options;
             $scope.toggleEnableButtons();
             $scope.newContentButton = "";
-
-
         };
         $scope.toggleEnableButtons = function() {
+            widgets = $vic('.vic-widget-container', '#vic-slot-' + $scope.slotId);
+            if (widgets.length == 0) {
+                var buttonBefore = angular.element('<new-widget-button position="" widget-map=""></new-widget-button>');
+                var templateBefore = $compile(buttonBefore);
+                $vic('#vic-slot-' + $scope.slotId).append(buttonBefore);
+                templateBefore($scope);
             }
         };
 
