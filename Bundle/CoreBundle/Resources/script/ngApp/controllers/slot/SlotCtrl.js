6165fb7e2b9c3a8f72e44d5551a3f33133fb7a79
Paul Andrieux <paul@appventus.com>
2016-01-28 11:36:47 +0100

    do not create new widget selects if max is reached

--- Bundle/CoreBundle/Resources/script/ngApp/controllers/slot/SlotCtrl.js
+++ Bundle/CoreBundle/Resources/script/ngApp/controllers/slot/SlotCtrl.js
@@ -9,11 +9,6 @@ angular.module('ngApp').controller("SlotController", ["$scope",
 
         };
         $scope.toggleEnableButtons = function() {
-            widgets = $('.vic-widget-container', '#vic-slot-' + $scope.slotId);
-            if (!("max" in $scope.options) || ("max" in $scope.options) && $scope.options.max > widgets.length) {
-                $('.vic-new-widget', '#vic-slot-' + $scope.slotId).removeClass('vic-new-widget-disabled');
-            } else {
-                $('.vic-new-widget', '#vic-slot-' + $scope.slotId).addClass('vic-new-widget-disabled');
             }
         };
 
