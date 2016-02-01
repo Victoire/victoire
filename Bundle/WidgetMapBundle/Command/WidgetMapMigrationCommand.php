a1095ac6d2168a36fd1cde77a5f2b596caf5ec4f
Paul Andrieux <paul@appventus.com>
2016-02-01 15:46:09 +0100

    remove var_dump from migration command

--- Bundle/WidgetMapBundle/Command/WidgetMapMigrationCommand.php
+++ Bundle/WidgetMapBundle/Command/WidgetMapMigrationCommand.php
@@ -96,7 +96,7 @@ class WidgetMapMigrationCommand extends ContainerAwareCommand
             if (!empty($oldWidgetMaps)) {
                 foreach ($oldWidgetMaps as $slot => $oldWidgetMap) {
                     $widgetMaps = [];
-                    var_dump($oldWidgetMap);
+                    $output->writeln(var_export($oldWidgetMap));
                     usort($oldWidgetMap, function($a, $b) {
                         if ($b['position'] - $a['position'] == 0) {
                             return 1;
@@ -104,7 +104,7 @@ class WidgetMapMigrationCommand extends ContainerAwareCommand
 
                         return $b['position'] - $a['position'];
                     });
-                    var_dump($oldWidgetMap);
+                    $output->writeln(var_export($oldWidgetMap));
 
                     foreach ($oldWidgetMap as $key => $item) {
                         if ($item['action'] !== 'create') {
@@ -124,36 +124,36 @@ class WidgetMapMigrationCommand extends ContainerAwareCommand
                         }
                     }
 
-                    var_dump($oldWidgetMap);
+                    $output->writeln(var_export($oldWidgetMap));
                     foreach ($oldWidgetMap as $position => $_oldWidgetMap) {
-                        var_dump('==========================');
-                        var_dump($slot);
-                        var_dump($_oldWidgetMap);
+                        $output->writeln('==========================');
+                        $output->writeln($slot);
+                        $output->writeln(var_export($_oldWidgetMap));
 
                         $widget = $widgetRepo->find($_oldWidgetMap['widgetId']);
                         if (!$widget) {
-                            var_dump('widget does not exists');
+                            $output->writeln('widget does not exists');
                             continue;
                         }
                         $widgetMap = new WidgetMap();
                         $referencedWidgetMap = null;
                         if ($_oldWidgetMap['positionReference'] != 0 && $_oldWidgetMap['positionReference'] != null) {
-                            var_dump('has positionReference');
+                            $output->writeln('has positionReference');
                             $referencedWidget = $widgetRepo->find($_oldWidgetMap['positionReference']);
-                            var_dump($referencedWidget->getId());
+                            $output->writeln($referencedWidget->getId());
                             $referencedWidgetMap = $view->getWidgetMapByWidget($referencedWidget);
                             while ($referencedWidgetMap->getChild(WidgetMap::POSITION_AFTER)) {
                                 $referencedWidgetMap = $referencedWidgetMap->getChild(WidgetMap::POSITION_AFTER);
                             }
-                            var_dump('set parent'.$referencedWidgetMap->getWidget()->getId());
+                            $output->writeln('set parent'.$referencedWidgetMap->getWidget()->getId());
                             $widgetMap->setParent($referencedWidgetMap);
                             $widgetMap->setPosition(WidgetMap::POSITION_AFTER);
                         } else {
-                            var_dump('has no positionReference');
+                            $output->writeln('has no positionReference');
                             if ($position == 0) {
                                 if (!isset($view->getBuiltWidgetMap()[$slot])) {
                                     $widgetMap->setPosition(null);
-                                    var_dump('set parent'.null);
+                                    $output->writeln('set parent'.null);
                                     $widgetMap->setParent(null);
                                 } else {
                                     $widgetMap->setPosition(WidgetMap::POSITION_BEFORE);
@@ -180,11 +180,11 @@ class WidgetMapMigrationCommand extends ContainerAwareCommand
                         }
 
                         if (WidgetMap::ACTION_OVERWRITE == $_oldWidgetMap['action']) {
-                            var_dump('is overwrite');
+                            $output->writeln('is overwrite');
 
                             /* @var Widget $replacedWidget */
                             if ($_oldWidgetMap['replacedWidgetId']) {
-                                var_dump('has replacedWidgetId');
+                                $output->writeln('has replacedWidgetId');
                                 $replacedWidget = $widgetRepo->find($_oldWidgetMap['replacedWidgetId']);
                                 $supplicantWidget = $widgetRepo->find($_oldWidgetMap['widgetId']);
                                 $replacedWidgetView = $replacedWidget->getView();
@@ -192,16 +192,16 @@ class WidgetMapMigrationCommand extends ContainerAwareCommand
                                 $replacedWidgetMap = $replacedWidgetView->getWidgetMapByWidget($replacedWidget);
                                 // If replaced widgetMap does not exists, this is not an overwrite but a create
                                 if ($replacedWidgetMap) {
-                                    var_dump('has replacedWidgetMap');
+                                    $output->writeln('has replacedWidgetMap');
                                     $widgetMap->setReplaced($replacedWidgetMap);
-                                    var_dump('replace '.$replacedWidget->getId().' by '.$supplicantWidget->getId());
+                                    $output->writeln('replace '.$replacedWidget->getId().' by '.$supplicantWidget->getId());
                                     $widgetMap->setWidget($supplicantWidget);
                                     $widgetMap->setPosition($replacedWidgetMap->getPosition());
-                                    var_dump('set parent'.($replacedWidgetMap->getParent() ? $replacedWidgetMap->getParent()->getWidget()->getId() : null));
+                                    $output->writeln('set parent'.($replacedWidgetMap->getParent() ? $replacedWidgetMap->getParent()->getWidget()->getId() : null));
                                     $widgetMap->setParent($replacedWidgetMap->getParent());
                                 }
                             } elseif ($referencedWidgetMap) {
-                                var_dump('move');
+                                $output->writeln('move');
 
                                 $this->getContainer()->get('victoire_widget_map.manager')->move($view, [
                                     'position'           => WidgetMap::POSITION_AFTER,
@@ -213,10 +213,10 @@ class WidgetMapMigrationCommand extends ContainerAwareCommand
                                 $_oldWidgetMap['action'] = WidgetMap::ACTION_CREATE;
                             }
                         } elseif (WidgetMap::ACTION_DELETE == $_oldWidgetMap['action']) {
-                            var_dump('is delete');
+                            $output->writeln('is delete');
                             $replacedWidget = $widgetRepo->find($_oldWidgetMap['widgetId']);
                             $widgetMap->setPosition(null);
-                            var_dump('set parent'.null);
+                            $output->writeln('set parent'.null);
                             $widgetMap->setParent(null);
                             $deletedWidgetMap = $view->getWidgetMapByWidget($replacedWidget);
                             if ($deletedWidgetMap) {
@@ -241,7 +241,7 @@ class WidgetMapMigrationCommand extends ContainerAwareCommand
                         $widgetMap->setWidget($widget);
                         $widgetMap->setAsynchronous($_oldWidgetMap['asynchronous'] ? true : false);
                         $widgetMap->setSlot($slot);
-                        var_dump('add widgetMap for widget '.$widgetMap->getWidget()->getId().' to view '.$view->getId());
+                        $output->writeln('add widgetMap for widget '.$widgetMap->getWidget()->getId().' to view '.$view->getId());
                         $view->addWidgetMap($widgetMap);
                         $em->persist($widgetMap);
                         $widgetMaps[] = $widgetMap;
