55d29c0df52c504fdcb31987bc9e892d1a406f83
Grégoire Humeau <gregoire@appventus.com>
2016-02-18 17:22:24 +0100

    Add limit and outofdate options to victoire:widget-css:generate command

--- Bundle/CoreBundle/Command/WidgetCssGenerateCommand.php
+++ Bundle/CoreBundle/Command/WidgetCssGenerateCommand.php
@@ -4,6 +4,7 @@ namespace Victoire\Bundle\CoreBundle\Command;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Victoire\Bundle\CoreBundle\Entity\View;
 
@@ -18,7 +19,9 @@ class WidgetCssGenerateCommand extends ContainerAwareCommand
 
         $this
             ->setName('victoire:widget-css:generate')
-            ->setDescription('Generate widgets css for each view');
+            ->addOption('outofdate', null, InputOption::VALUE_NONE, 'Generate CSS only for View\'s CSS not up to date')
+            ->addOption('limit', 'l', InputOption::VALUE_REQUIRED, 'Number of generated View\'s CSS')
+            ->setDescription('Generate widgets CSS for each View');
     }
 
     /**
@@ -27,38 +30,67 @@ class WidgetCssGenerateCommand extends ContainerAwareCommand
      * @param InputInterface  $input
      * @param OutputInterface $output
      *
-     * @return void
+     * @throws \Exception
+     *
+     * @return boolean
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $progress = $this->getHelper('progress');
+        //Get options
+        $outOfDate = $input->getOption('outofdate');
+        $limit = $input->getOption('limit');
 
+        //Get services
         $entityManager = $this->getContainer()->get('doctrine.orm.entity_manager');
-        $viewRepo = $entityManager->getRepository('Victoire\Bundle\CoreBundle\Entity\View');
-        $widgetRepo = $entityManager->getRepository('Victoire\Bundle\WidgetBundle\Entity\Widget');
-
         $viewCssBuilder = $this->getContainer()->get('victoire_core.view_css_builder');
         $widgetMapBuilder = $this->getContainer()->get('victoire_widget_map.builder');
 
-        $viewCssBuilder->clearViewCssFolder();
-        $views = $viewRepo->findAll();
-        $progress->start($output, count($views));
+        //Get repositories
+        $viewRepo = $entityManager->getRepository('Victoire\Bundle\CoreBundle\Entity\View');
+        $widgetRepo = $entityManager->getRepository('Victoire\Bundle\WidgetBundle\Entity\Widget');
 
+        //Get View's CSS to generate
         /* @var $views View[] */
+        $views = $outOfDate ? $viewRepo->findByCssUpToDate(false) : $viewRepo->findAll();
+
+        //Prepare limit
+        $limit = ($limit && $limit < count($views)) ? $limit : count($views);
+        $count = 0;
+
+        //Set progress for output
+        $progress = $this->getHelper('progress');
+        $progress->start($output, $limit);
+
         foreach ($views as $view) {
-            if (!$viewHash = $view->getCssHash()) {
-                $view->changeCssHash();
-                $entityManager->persist($view);
-                $entityManager->flush($view);
+
+            //Exit if limit reached
+            if ($count >= $limit) {
+                break;
             }
 
+            //If hash already exist, remove CSS file, else generate a hash
+            if ($viewHash = $view->getCssHash()) {
+                $viewCssBuilder->removeCssFile($viewHash);
+            } else {
+                $view->changeCssHash();
+            }
+
+            //Generate CSS file with its widgets style
             $widgetMapBuilder->build($view, true);
             $widgets = $widgetRepo->findAllWidgetsForView($view);
             $viewCssBuilder->generateViewCss($view, $widgets);
 
+            //Set View's CSS as up to date
+            $view->setCssUpToDate(true);
+            $entityManager->persist($view);
+            $entityManager->flush($view);
+
+            ++$count;
             $progress->advance();
         }
 
         $progress->finish();
+
+        return true;
     }
 }
