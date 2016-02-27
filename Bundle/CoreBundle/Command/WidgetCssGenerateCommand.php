c2cba1a8cf7f953a66035ae46c9eb9cdeab85089
Paul Andrieux <paul@appventus.com>
2016-02-27 22:04:35 +0100

    get pages in separate queries instead of using the ViewRepository to avoid lazy loading performence issue

--- Bundle/CoreBundle/Command/WidgetCssGenerateCommand.php
+++ Bundle/CoreBundle/Command/WidgetCssGenerateCommand.php
@@ -2,11 +2,13 @@
 
 namespace Victoire\Bundle\CoreBundle\Command;
 
+use Doctrine\ORM\EntityManager;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Victoire\Bundle\CoreBundle\Entity\View;
+use Victoire\Bundle\TemplateBundle\Entity\Template;
 
 class WidgetCssGenerateCommand extends ContainerAwareCommand
 {
@@ -41,17 +43,41 @@ class WidgetCssGenerateCommand extends ContainerAwareCommand
         $limit = $input->getOption('limit');
 
         //Get services
+
+        /** @var EntityManager $entityManager */
         $entityManager = $this->getContainer()->get('doctrine.orm.entity_manager');
         $viewCssBuilder = $this->getContainer()->get('victoire_core.view_css_builder');
         $widgetMapBuilder = $this->getContainer()->get('victoire_widget_map.builder');
 
-        //Get repositories
-        $viewRepo = $entityManager->getRepository('Victoire\Bundle\CoreBundle\Entity\View');
         $widgetRepo = $entityManager->getRepository('Victoire\Bundle\WidgetBundle\Entity\Widget');
 
-        //Get View's CSS to generate
+//        //Get View's CSS to generate
+        $templateRepo = $entityManager->getRepository('VictoireTemplateBundle:Template');
+        $rootTemplates = $templateRepo->getInstance()
+            ->where('template.template IS NULL')
+            ->getQuery()
+            ->getResult();
+        $templates = [];
+        $recursiveGetTemplates = function ($template) use (&$recursiveGetTemplates, &$templates) {
+            array_push($templates, $template);
+            foreach ($template->getInheritors() as $template) {
+                if ($template instanceof Template) {
+                    $recursiveGetTemplates($template);
+                }
+            }
+        };
+
+        foreach ($rootTemplates as $rootTemplate) {
+            $recursiveGetTemplates($rootTemplate);
+        }
+
+        $pageRepo = $entityManager->getRepository('VictoirePageBundle:BasePage');
+        $pages = $pageRepo->findAll();
+        $errorRepo = $entityManager->getRepository('VictoireTwigBundle:ErrorPage');
+        $errorPages = $errorRepo->findAll();
+
         /* @var $views View[] */
-        $views = $outOfDate ? $viewRepo->findByCssUpToDate(false) : $viewRepo->findAll();
+        $views = array_merge($templates, array_merge($pages, $errorPages));
 
         //Prepare limit
         $limit = ($limit && $limit < count($views)) ? $limit : count($views);
@@ -62,6 +88,9 @@ class WidgetCssGenerateCommand extends ContainerAwareCommand
         $progress->start($output, $limit);
 
         foreach ($views as $view) {
+            if ($outOfDate && $view->isCssUpToDate()) {
+                continue;
+            }
 
             //Exit if limit reached
             if ($count >= $limit) {
@@ -76,7 +105,7 @@ class WidgetCssGenerateCommand extends ContainerAwareCommand
             }
 
             //Generate CSS file with its widgets style
-            $widgetMapBuilder->build($view, true);
+            $widgetMapBuilder->build($view, $entityManager);
             $widgets = $widgetRepo->findAllWidgetsForView($view);
             $viewCssBuilder->generateViewCss($view, $widgets);
 
