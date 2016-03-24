3c6bdaf3dc5e12169ffca22bb5f2ed46ad801a55
Leny BERNARD <leny@appventus.com>
2016-03-24 23:37:10 +0100

    viewReference => viewReferences to allow to store different locales viewReferences

--- Bundle/PageBundle/EventSubscriber/PageSubscriber.php
+++ Bundle/PageBundle/EventSubscriber/PageSubscriber.php
@@ -3,6 +3,7 @@
 namespace Victoire\Bundle\PageBundle\EventSubscriber;
 
 use Doctrine\Common\EventSubscriber;
+use Doctrine\Common\Util\ClassUtils;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Event\LifecycleEventArgs;
 use Doctrine\ORM\Event\LoadClassMetadataEventArgs;
@@ -13,6 +14,8 @@ use Symfony\Bundle\FrameworkBundle\Routing\Router;
 use Victoire\Bundle\CoreBundle\Entity\View;
 use Victoire\Bundle\CoreBundle\Entity\WebViewInterface;
 use Victoire\Bundle\PageBundle\Helper\UserCallableHelper;
+use Victoire\Bundle\TemplateBundle\Entity\Template;
+use Victoire\Bundle\TwigBundle\Entity\ErrorPage;
 use Victoire\Bundle\ViewReferenceBundle\Builder\ViewReferenceBuilder;
 use Victoire\Bundle\ViewReferenceBundle\Connector\ViewReferenceRepository;
 use Victoire\Bundle\ViewReferenceBundle\ViewReference\ViewReference;
@@ -131,12 +134,17 @@ class PageSubscriber implements EventSubscriber
         if ($entity instanceof View) {
             $viewReference = $this->viewReferenceRepository->getOneReferenceByParameters([
                 'viewId' => $entity->getId(),
+                'locale' => $entity->getLocale()
             ]);
-            if ($viewReference instanceof ViewReference && $entity instanceof WebViewInterface) {
-                $entity->setReference($viewReference);
+            if ($entity instanceof WebViewInterface && $viewReference instanceof ViewReference) {
+                $entity->setReferences([$entity->getLocale() => $viewReference]);
                 $entity->setUrl($viewReference->getUrl());
+            } elseif ($entity instanceof Template || $entity instanceof ErrorPage) {
+                $entity->setReferences([$entity->getLocale() => new ViewReference($entity->getId())]);
             } else {
-                $entity->setReference(new ViewReference($entity->getId()));
+                $entity->setReferences([
+                    $entity->getLocale() => $this->viewReferenceBuilder->buildViewReference($entity, $eventArgs->getEntityManager())
+                ]);
             }
         }
     }
