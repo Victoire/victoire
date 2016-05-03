b70314deae763798e1643f51aa28e8f7ce8ee0a9
Charlie <charlielucas.cl@gmail.com>
2016-05-03 10:43:48 +0200

    allow to pass directly the entity id in find BP method in BusinessPage Repository

--- Bundle/BusinessPageBundle/Repository/BusinessPageRepository.php
+++ Bundle/BusinessPageBundle/Repository/BusinessPageRepository.php
@@ -12,14 +12,19 @@ use Victoire\Bundle\BusinessPageBundle\Entity\BusinessTemplate;
 class BusinessPageRepository extends EntityRepository
 {
     /**
-     * Find the pagePatterns of the business entity.
+     *  Find the pagePatterns of the business entity.
      *
-     * @param BusinessEntity $businessEntity
+     * @param BusinessTemplate $pattern
+     * @param object           $entity         | int $entityId
+     * @param BusinessEntity   $businessEntity
      *
-     * @return array The list of pagePatterns
+     * @return mixed
      */
     public function findPageByBusinessEntityAndPattern(BusinessTemplate $pattern, $entity, BusinessEntity $businessEntity)
     {
+        if (is_object($entity)) {
+            $entity = $entity->getId();
+        }
         $qb = $this->createQueryBuilder('BusinessPage');
         $qb->join('BusinessPage.entityProxy', 'proxy');
         $qb->join('BusinessPage.template', 'template');
@@ -30,7 +35,7 @@ class BusinessPageRepository extends EntityRepository
         $qb->andWhere('entity.id = :entityId');
 
         $qb->setParameter(':templateId', $pattern);
-        $qb->setParameter(':entityId', $entity->getId());
+        $qb->setParameter(':entityId', $entity);
         $result = $qb->getQuery()->getOneOrNullResult();
 
         return $result;
