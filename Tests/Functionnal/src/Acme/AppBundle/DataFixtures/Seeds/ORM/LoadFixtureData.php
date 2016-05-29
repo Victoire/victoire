01a2f0034a341e038485076049c6979562014b83
Paul Andrieux <paul@appventus.com>
2016-05-29 23:18:33 +0200

    remove dead code

--- Tests/Functionnal/src/Acme/AppBundle/DataFixtures/Seeds/ORM/LoadFixtureData.php
+++ Tests/Functionnal/src/Acme/AppBundle/DataFixtures/Seeds/ORM/LoadFixtureData.php
@@ -52,12 +52,6 @@ class LoadFixtureData extends AbstractFixture implements ContainerAwareInterface
         ]
     );
 
-//        foreach ($objects as $object) {
-//            if (in_array(Translatable::class, class_uses($object))) {
-//                $object->mergeNewTranslations();
-//            }
-//        }
-
         $manager->flush();
     }
 
