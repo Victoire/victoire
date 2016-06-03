cb677e95e4e7af6bd506a342c17f80a2cb8b2423
Leny BERNARD <leny@appventus.com>
2016-06-03 17:14:56 +0200

    allow to use specific strategy in data-refresh-on-change

--- Bundle/FormBundle/Resources/public/js/form.js
+++ Bundle/FormBundle/Resources/public/js/form.js
@@ -21,6 +21,10 @@ $vic(document).on('change', 'select[data-refreshOnChange="true"], input:checkbox
     } else if ($vic(this).parents('.vic-modal').hasClass('-stylize')) {
         targetClass = ".vic-modal-body .vic-container-fluid";
     }
+    var updateStrategy = "html";
+    if ($vic(this).data('update-strategy')) {
+        updateStrategy = $vic(this).data('update-strategy');
+    }
 
     //Get last element of visible target class
     var $target = $vic(document).find(targetClass + ':visible');
@@ -32,7 +36,10 @@ $vic(document).on('change', 'select[data-refreshOnChange="true"], input:checkbox
         data: form.serialize(),
         async: true
     }).done(function(response){
-        $target.html(response.html);
+
+        //By default, the updateStrategy is html (a simple replace) but you can set your own function
+        //for example, append, after etc or even a custom one.
+        eval('$target.' + updateStrategy + '(response.html)');
         var scripts = $target.find("script");
         loading(false);
     }).fail(function(response) {
