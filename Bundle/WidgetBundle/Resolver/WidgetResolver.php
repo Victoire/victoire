29197a5cf235e14b5a21a2599c321569a5a44983
made wilson <a.grandin42@gmail.com>
2016-06-23 17:13:25 +0200

    Create RolesDataSource use as datasource criteria to test user role Add is_granted assertion in CriteriaType Add datasource in service.yml Add translation in fr en es for Roles input Correct bug in WidgetResolver was returning widget if true or false Add authorizationChecker as dependencies of widgetResolver in service.yml

--- Bundle/WidgetBundle/Resolver/WidgetResolver.php
+++ Bundle/WidgetBundle/Resolver/WidgetResolver.php
@@ -8,6 +8,7 @@
 namespace Victoire\Bundle\WidgetBundle\Resolver;
 
 use Symfony\Component\PropertyAccess\PropertyAccessor;
+use Symfony\Component\Security\Core\Authorization\AuthorizationChecker;
 use Victoire\Bundle\CriteriaBundle\Chain\DataSourceChain;
 use Victoire\Bundle\CriteriaBundle\Entity\Criteria;
 use Victoire\Bundle\WidgetBundle\Entity\Widget;
@@ -17,20 +18,24 @@ class WidgetResolver
 {
     const OPERAND_EQUAL = 'equal';
     const OPERAND_IN = 'in';
+    const IS_GRANTED = 'is_granted';
 
     /**
      * @var DataSourceChain
      */
     private $dataSourceChain;
 
+    private $authorizationChecker;
+
     /**
      * WidgetResolver constructor.
-     *
      * @param DataSourceChain $dataSourceChain
+     * @param AuthorizationChecker $authorizationChecker
      */
-    public function __construct(DataSourceChain $dataSourceChain)
+    public function __construct(DataSourceChain $dataSourceChain, AuthorizationChecker $authorizationChecker)
     {
         $this->dataSourceChain = $dataSourceChain;
+        $this->authorizationChecker = $authorizationChecker;
     }
 
     public function resolve(WidgetMap $widgetMap)
@@ -61,6 +66,9 @@ class WidgetResolver
                 break;
             case self::OPERAND_IN:
                 $result = in_array($value, unserialize($expected));
+                break;
+            case self::IS_GRANTED:
+                $result = $this->authorizationChecker->isGranted($expected);
                 break;
         }
 
