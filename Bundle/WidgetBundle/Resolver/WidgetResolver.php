e85398ff52576c806484f9a3c74eeedd3f835921
MadeWilson <a.grandin42@gmail.com>
2016-07-11 15:00:56 +0200

    Apply patch StyleCI

--- Bundle/WidgetBundle/Resolver/WidgetResolver.php
+++ Bundle/WidgetBundle/Resolver/WidgetResolver.php
@@ -32,7 +32,8 @@ class WidgetResolver
 
     /**
      * WidgetResolver constructor.
-     * @param DataSourceChain $dataSourceChain
+     *
+     * @param DataSourceChain      $dataSourceChain
      * @param AuthorizationChecker $authorizationChecker
      */
     public function __construct(DataSourceChain $dataSourceChain, AuthorizationChecker $authorizationChecker)
