7a5b3544a7854b705d704c8373f3846c989c31a2
Paul Andrieux <paul@appventus.com>
2016-07-20 10:52:26 +0200

    make the PageSeo translatable

--- Bundle/SeoBundle/Form/PageSeoType.php
+++ Bundle/SeoBundle/Form/PageSeoType.php
@@ -2,11 +2,14 @@
 
 namespace Victoire\Bundle\SeoBundle\Form;
 
+use A2lix\TranslationFormBundle\Form\Type\TranslationsType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Victoire\Bundle\FormBundle\Form\Type\LinkType;
 use Victoire\Bundle\MediaBundle\Form\Type\MediaType;
+use Victoire\Bundle\SeoBundle\Entity\PageSeo;
 
 class PageSeoType extends AbstractType
 {
@@ -17,146 +20,180 @@ class PageSeoType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('metaTitle', null, [
-                'label' => 'form.pageSeo.metaTitle.label',
-                'attr'  => ['placeholder' => 'form.pageSeo.metaTitle.placeholder'],
-            ])
-            ->add('metaDescription', null, [
-                'label' => 'form.pageSeo.metaDescription.label',
-                'attr'  => ['placeholder' => 'form.pageSeo.metaDescription.placeholder'],
-            ])
-            ->add('relAuthor', null, [
-                'label' => 'form.pageSeo.relAuthor.label',
-                'attr'  => ['placeholder' => 'form.pageSeo.relAuthor.placeholder'],
-            ])
-            ->add('relPublisher', null, [
-                'label' => 'form.pageSeo.relPublisher.label',
-                'attr'  => ['placeholder' => 'form.pageSeo.relPublisher.placeholder'],
-            ])
-            ->add('ogTitle', null, [
-                'label'          => 'form.pageSeo.ogTitle.label',
-                'vic_help_block' => 'form.pageSeo.ogTitle.vic_help_block',
-            ])
-            ->add('ogType', null, [
-                'label' => 'form.pageSeo.ogType.label',
-                'attr'  => ['placeholder' => 'form.pageSeo.ogType.placeholder'],
-            ])
-            ->add('ogImage', MediaType::class, [
-                'label' => 'form.pageSeo.ogImage.label',
-            ])
-            ->add('ogUrl', null, [
-                'label' => 'form.pageSeo.ogUrl.label',
-                'attr'  => ['placeholder' => 'form.pageSeo.ogUrl.placeholder'],
-            ])
-            ->add('ogDescription', null, [
-                'label' => 'form.pageSeo.ogDescription.label',
-            ])
-            ->add('fbAdmins', null, [
-                'label' => 'form.pageSeo.fbAdmins.label',
-                'attr'  => ['placeholder' => 'form.pageSeo.fbAdmins.placeholder'],
-            ])
-            ->add('twitterCard', ChoiceType::class,
-                [
-                    'label'   => 'form.pageSeo.twitterCard.label',
-                    'choices' => [
-                        'form.pageSeo.twitterCard.summary.label'             => 'summary',
-                        'form.pageSeo.twitterCard.summary_large_image.label' => 'summary_large_image',
-                        'form.pageSeo.twitterCard.photo.label'               => 'photo',
-                        'form.pageSeo.twitterCard.app.label'                 => 'app',
-                        'form.pageSeo.twitterCard.player.label'              => 'player',
-                        'form.pageSeo.twitterCard.product.label'             => 'product',
+            ->add('translations', TranslationsType::class, [
+                'fields' => [
+                    'metaTitle' => [
+                        'label' => 'form.pageSeo.metaTitle.label',
+                        'attr'  => [
+                            'placeholder' => 'form.pageSeo.metaTitle.placeholder'
+                        ],
                     ],
-                    'choices_as_values' => true,
-                    'preferred_choices' => ['summary'],
-                    'vic_help_block'    => 'form.pageSeo.twitterCard.vic_help_block',
-            ])
-            ->add('twitterUrl', null, [
-                'label' => 'form.pageSeo.twitterUrl.label',
-                'attr'  => ['placeholder' => 'form.pageSeo.twitterUrl.placeholder'],
-            ])
-            ->add('twitterTitle', null, [
-                'label' => 'form.pageSeo.twitterTitle.label',
-                'attr'  => ['placeholder' => 'form.pageSeo.twitterTitle.placeholder'],
-            ])
-            ->add('twitterDescription', null, [
-                'label' => 'form.pageSeo.twitterDescription.label',
-                'attr'  => ['placeholder' => 'form.pageSeo.twitterDescription.placeholder'],
-            ])
-            ->add('twitterCreator', null, [
-                'label' => 'form.pageSeo.twitterCreator.label',
-                'attr'  => ['placeholder' => 'form.pageSeo.twitterCreator.placeholder'],
-            ])
-            ->add('twitterImage', MediaType::class, [
-                'label' => 'form.pageSeo.twitterImage.label',
-            ])
-            ->add('schemaPageType', null, [
-                'label'          => 'form.pageSeo.schemaPageType.label',
-                'vic_help_block' => 'form.pageSeo.schemaPageType.vic_help_block',
-            ])
-            ->add('schemaName', null, [
-                'label' => 'form.pageSeo.schemaName.label',
-            ])
-            ->add('schemaDescription', null, [
-                'label' => 'form.pageSeo.schemaDescription.label',
-            ])
-            ->add('schemaImage', MediaType::class, [
-                'label' => 'form.pageSeo.schemaImage.label',
-            ])
-            ->add('metaRobotsIndex', ChoiceType::class, [
-                'choices_as_values' => true,
-                'label'             => 'form.pageSeo.metaRobotsIndex.label',
-                'choices'           => [
-                    'form.pageSeo.metaRobotsIndex.values.index'   => 'index',
-                    'form.pageSeo.metaRobotsIndex.values.noindex' => 'noindex',
-                ],
-            ])
-            ->add('metaRobotsFollow', ChoiceType::class, [
-                'choices_as_values' => true,
-                'label'             => 'form.pageSeo.metaRobotsFollow.label',
-                'choices'           => [
-                    'form.pageSeo.metaRobotsFollow.values.follow'   => 'follow',
-                    'form.pageSeo.metaRobotsFollow.values.nofollow' => 'nofollow',
-                ],
-            ])
-            ->add('metaRobotsAdvanced', null, [
-                'label' => 'form.pageSeo.metaRobotsAdvanced.label',
-            ])
-            ->add('sitemapIndexed', null, [
-                'label' => 'form.pageSeo.sitemapIndexed.label',
-            ])
-            ->add('sitemapChangeFreq', ChoiceType::class,
-                [
-                    'label'   => 'form.pageSeo.sitemapChangeFreq.label',
-                    'choices' => [
-                        'form.pageSeo.sitemapChangeFreq.format.choices.always.label'  => 'always',
-                        'form.pageSeo.sitemapChangeFreq.format.choices.hourly.label'  => 'hourly',
-                        'form.pageSeo.sitemapChangeFreq.format.choices.daily.label'   => 'daily',
-                        'form.pageSeo.sitemapChangeFreq.format.choices.weekly.label'  => 'weekly',
-                        'form.pageSeo.sitemapChangeFreq.format.choices.monthly.label' => 'monthly',
-                        'form.pageSeo.sitemapChangeFreq.format.choices.yearly.label'  => 'yearly',
-                        'form.pageSeo.sitemapChangeFreq.format.choices.never.label'   => 'never',
+                    'metaDescription' => [
+                        'label' => 'form.pageSeo.metaDescription.label',
+                        'attr'  => [
+                            'placeholder' => 'form.pageSeo.metaDescription.placeholder'
+                        ],
                     ],
-                    'choices_as_values' => true,
-                    'preferred_choices' => ['monthly'],
-            ])
-            ->add('sitemapPriority', null, [
-                'label' => 'form.pageSeo.sitemapPriority.label',
-            ])
-            ->add('sitemapPriority', ChoiceType::class, [
-                'label'             => 'form.pageSeo.sitemapPriority.label',
-                'choices'           => array_combine(range(0, 1, 0.1), range(0, 1, 0.1)),
-                'choices_as_values' => true,
-            ])
-            ->add('relCanonical', null, [
-                'label' => 'form.pageSeo.relCanonical.label',
-            ])
-            ->add('keyword', null, [
-                'label' => 'form.pageSeo.keyword.label',
-                'attr'  => ['placeholder' => 'form.pageSeo.keyword.placeholder'],
-            ])
-            ->add('redirectTo', null, [
-                'label'          => 'form.pageSeo.redirectTo.label',
-                'vic_help_block' => 'form.pageSeo.redirectTo.vic_help_block',
+                    'relAuthor' => [
+                        'label' => 'form.pageSeo.relAuthor.label',
+                        'attr'  => [
+                            'placeholder' => 'form.pageSeo.relAuthor.placeholder'
+                        ],
+                    ],
+                    'relPublisher' => [
+                        'label' => 'form.pageSeo.relPublisher.label',
+                        'attr'  => [
+                            'placeholder' => 'form.pageSeo.relPublisher.placeholder'
+                        ],
+                    ],
+                    'ogTitle' => [
+                        'label'          => 'form.pageSeo.ogTitle.label',
+                        'vic_help_block' => 'form.pageSeo.ogTitle.vic_help_block',
+                    ],
+                    'ogType' => [
+                        'label' => 'form.pageSeo.ogType.label',
+                        'attr'  => [
+                            'placeholder' => 'form.pageSeo.ogType.placeholder'
+                        ],
+                    ],
+                    'ogImage' => [
+                        'field_type' => MediaType::class,
+                        'label' => 'form.pageSeo.ogImage.label',
+                    ],
+                    'ogUrl' => [
+                        'label' => 'form.pageSeo.ogUrl.label',
+                        'attr'  => [
+                            'placeholder' => 'form.pageSeo.ogUrl.placeholder'
+                        ],
+                    ],
+                    'ogDescription' => [
+                        'label' => 'form.pageSeo.ogDescription.label',
+                    ],
+                    'fbAdmins' => [
+                        'label' => 'form.pageSeo.fbAdmins.label',
+                        'attr'  => [
+                            'placeholder' => 'form.pageSeo.fbAdmins.placeholder'
+                        ],
+                    ],
+                    'twitterCard' =>
+                        [
+                            'field_type' => ChoiceType::class,
+                            'label'   => 'form.pageSeo.twitterCard.label',
+                            'choices' => [
+                                'form.pageSeo.twitterCard.summary.label'             => 'summary',
+                                'form.pageSeo.twitterCard.summary_large_image.label' => 'summary_large_image',
+                                'form.pageSeo.twitterCard.photo.label'               => 'photo',
+                                'form.pageSeo.twitterCard.app.label'                 => 'app',
+                                'form.pageSeo.twitterCard.player.label'              => 'player',
+                                'form.pageSeo.twitterCard.product.label'             => 'product',
+                            ],
+                            'choices_as_values' => true,
+                            'preferred_choices' => ['summary'],
+                            'vic_help_block'    => 'form.pageSeo.twitterCard.vic_help_block',
+                        ],
+                    'twitterUrl' => [
+                        'label' => 'form.pageSeo.twitterUrl.label',
+                        'attr'  => [
+                            'placeholder' => 'form.pageSeo.twitterUrl.placeholder'
+                        ],
+                    ],
+                    'twitterTitle' => [
+                        'label' => 'form.pageSeo.twitterTitle.label',
+                        'attr'  => [
+                            'placeholder' => 'form.pageSeo.twitterTitle.placeholder'
+                        ],
+                    ],
+                    'twitterDescription' => [
+                        'label' => 'form.pageSeo.twitterDescription.label',
+                        'attr'  => [
+                            'placeholder' => 'form.pageSeo.twitterDescription.placeholder'
+                        ],
+                    ],
+                    'twitterCreator' => [
+                        'label' => 'form.pageSeo.twitterCreator.label',
+                        'attr'  => [
+                            'placeholder' => 'form.pageSeo.twitterCreator.placeholder'
+                        ],
+                    ],
+                    'twitterImage' => [
+                        'field_type' => MediaType::class,
+                        'label' => 'form.pageSeo.twitterImage.label',
+                    ],
+                    'schemaPageType' => [
+                        'label'          => 'form.pageSeo.schemaPageType.label',
+                        'vic_help_block' => 'form.pageSeo.schemaPageType.vic_help_block',
+                    ],
+                    'schemaName' => [
+                        'label' => 'form.pageSeo.schemaName.label',
+                    ],
+                    'schemaDescription' => [
+                        'label' => 'form.pageSeo.schemaDescription.label',
+                    ],
+                    'schemaImage' => [
+                        'field_type' => MediaType::class,
+                        'label' => 'form.pageSeo.schemaImage.label',
+                    ],
+                    'metaRobotsIndex' => [
+                        'field_type' => ChoiceType::class,
+                        'choices_as_values' => true,
+                        'label'             => 'form.pageSeo.metaRobotsIndex.label',
+                        'choices'           => [
+                            'form.pageSeo.metaRobotsIndex.values.index'   => 'index',
+                            'form.pageSeo.metaRobotsIndex.values.noindex' => 'noindex',
+                        ],
+                    ],
+                    'metaRobotsFollow' => [
+                        'field_type' => ChoiceType::class,
+                        'choices_as_values' => true,
+                        'label'             => 'form.pageSeo.metaRobotsFollow.label',
+                        'choices'           => [
+                            'form.pageSeo.metaRobotsFollow.values.follow'   => 'follow',
+                            'form.pageSeo.metaRobotsFollow.values.nofollow' => 'nofollow',
+                        ],
+                    ],
+                    'metaRobotsAdvanced' => [
+                        'label' => 'form.pageSeo.metaRobotsAdvanced.label',
+                    ],
+                    'sitemapIndexed' => [
+                        'label' => 'form.pageSeo.sitemapIndexed.label',
+                    ],
+                    'sitemapChangeFreq' => [
+                        'field_type' => ChoiceType::class,
+                        'label'   => 'form.pageSeo.sitemapChangeFreq.label',
+                        'choices' => [
+                            'form.pageSeo.sitemapChangeFreq.format.choices.always.label'  => 'always',
+                            'form.pageSeo.sitemapChangeFreq.format.choices.hourly.label'  => 'hourly',
+                            'form.pageSeo.sitemapChangeFreq.format.choices.daily.label'   => 'daily',
+                            'form.pageSeo.sitemapChangeFreq.format.choices.weekly.label'  => 'weekly',
+                            'form.pageSeo.sitemapChangeFreq.format.choices.monthly.label' => 'monthly',
+                            'form.pageSeo.sitemapChangeFreq.format.choices.yearly.label'  => 'yearly',
+                            'form.pageSeo.sitemapChangeFreq.format.choices.never.label'   => 'never',
+                        ],
+                        'choices_as_values' => true,
+                        'preferred_choices' => ['monthly'],
+                    ],
+                    'sitemapPriority' => [
+                        'field_type' => ChoiceType::class,
+                        'label'             => 'form.pageSeo.sitemapPriority.label',
+                        'choices'           => array_combine(range(0, 1, 0.1), range(0, 1, 0.1)),
+                        'choices_as_values' => true,
+                    ],
+                    'relCanonical' => [
+                        'label' => 'form.pageSeo.relCanonical.label',
+                    ],
+                    'keyword' => [
+                        'label' => 'form.pageSeo.keyword.label',
+                        'attr'  => [
+                            'placeholder' => 'form.pageSeo.keyword.placeholder'
+                        ],
+                    ],
+                    'redirectTo' => [
+                        'field_type' => LinkType::class,
+                        'refresh-target' => '#basics',
+                        'label'          => 'form.pageSeo.redirectTo.label',
+                        'vic_help_block' => 'form.pageSeo.redirectTo.vic_help_block',
+                    ]
+                ]
             ]);
     }
 
@@ -169,5 +206,12 @@ class PageSeoType extends AbstractType
             'data_class'         => 'Victoire\Bundle\SeoBundle\Entity\PageSeo',
             'translation_domain' => 'victoire',
         ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix() {
+        return 'victoire_page_seo';
     }
 }
