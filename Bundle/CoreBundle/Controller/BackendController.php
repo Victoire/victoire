dd57c55edc0adde02d6bc1ddc6a119b8f333e8d5
Paul Andrieux <paul@appventus.com>
2016-11-10 12:19:47 +0100

    avoid public method in controller

--- Bundle/CoreBundle/Controller/BackendController.php
+++ Bundle/CoreBundle/Controller/BackendController.php
@@ -3,7 +3,6 @@
 namespace Victoire\Bundle\CoreBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\EventDispatcher\Event;
 
 /**
  * Backend controller.
@@ -12,14 +11,4 @@ abstract class BackendController extends Controller
 {
     use VictoireAlertifyControllerTrait;
 
-    /**
-     * This method is run for each backend request by the victoire_core.controller.pre_execute_listener
-     * Must be public to be run outside in ControllerListener.
-     *
-     * @return void
-     */
-    public function preExecute()
-    {
-        $this->get('event_dispatcher')->dispatch('victoire_core.backend_menu.global', new Event());
-    }
 }
