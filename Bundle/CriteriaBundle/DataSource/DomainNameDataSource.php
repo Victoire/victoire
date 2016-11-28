37986232c1da2b4247a0770bb47b7bdd4ebfb9a5
Made Wilson <anthony@troopers.email>
2016-11-28 15:04:22 +0100

    Create methode getCurrentDomainName return header host frome request stack has to be compare with equal assertion to domainName provide in victoire_core config file
    Declare DomainNameDatasource has service

--- Bundle/CriteriaBundle/DataSource/DomainNameDataSource.php
+++ Bundle/CriteriaBundle/DataSource/DomainNameDataSource.php
@@ -1,15 +1,69 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: made
- * Date: 28/11/16
- * Time: 13:42
- */
 
 namespace Victoire\Bundle\CriteriaBundle\DataSource;
 
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\HttpFoundation\RequestStack;
 
+/**
+ * Class DomainNameDataSource
+ * @package Victoire\Bundle\CriteriaBundle\DataSource
+ */
 class DomainNameDataSource
 {
+    /**
+     * @var array
+     */
+    private $domainName;
+    /**
+     * @var RequestStack
+     */
+    private $requestStack;
 
+    /**
+     * DomainNameDataSource constructor.
+     * @param array $domainName
+     * @param RequestStack $requestStack
+     */
+    public function __construct(
+        array $domainName,
+        RequestStack $requestStack
+    )
+    {
+        $this->domainName = $domainName;
+        $this->requestStack = $requestStack;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCurrentDomainName()
+    {
+        return $this->requestStack->getCurrentRequest()->getHost();
+    }
+
+    /**
+     * @return array
+     */
+    public function getDomainName()
+    {
+        return $this->domainName[0];
+    }
+
+    /**
+     * @return array
+     */
+    public function getCurrentDomainNameFormParams()
+    {
+        return [
+            'type' => ChoiceType::class,
+            'options' => [
+                'choices' => $this->getDomainName(),
+                'choices_as_values' => true,
+                'choice_label'      => function ($value) {
+                    return $value;
+                },
+            ]
+        ];
+    }
 }
