5663eb301ca6a65e893377f0605ef89e43bddaa2
anthony <a.grandin42@gmail.com>
2016-11-29 16:27:50 +0100

    Feature/#34 criteria domain name (#702)
    * Add domain_name node in victoire_core configuration
    
    * Create new DataSource DomainNameDataSource.php
    Correct bad positionning for node domain_name in Configuration.php
    Add missing declaration for domain_name parameter in VictoireCoreExtension.php
    
    * Create methode getCurrentDomainName return header host frome request stack has to be
    compare with equal assertion to domainName provide in victoire_core config file
    
    Declare DomainNameDatasource has service
    
    * Add translation for domain name criteria label in victoire
    
    * apply style ci

---
+++ Bundle/CriteriaBundle/DataSource/DomainNameDataSource.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Victoire\Bundle\CriteriaBundle\DataSource;
+
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+/**
+ * Class DomainNameDataSource.
+ */
+class DomainNameDataSource
+{
+    /**
+     * @var array
+     */
+    private $domainName;
+    /**
+     * @var RequestStack
+     */
+    private $requestStack;
+
+    /**
+     * DomainNameDataSource constructor.
+     *
+     * @param array        $domainName
+     * @param RequestStack $requestStack
+     */
+    public function __construct(
+        array $domainName,
+        RequestStack $requestStack
+    ) {
+        $this->domainName = $domainName;
+        $this->requestStack = $requestStack;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCurrentDomainName()
+    {
+        return $this->requestStack->getCurrentRequest()->getHost();
+    }
+
+    /**
+     * @return array
+     */
+    public function getDomainName()
+    {
+        return $this->domainName[0];
+    }
+
+    /**
+     * @return array
+     */
+    public function getCurrentDomainNameFormParams()
+    {
+        return [
+            'type'    => ChoiceType::class,
+            'options' => [
+                'choices'           => $this->getDomainName(),
+                'choices_as_values' => true,
+                'choice_label'      => function ($value) {
+                    return $value;
+                },
+            ],
+        ];
+    }
+}
