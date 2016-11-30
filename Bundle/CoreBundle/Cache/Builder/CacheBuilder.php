72d12af205fd355fd81e0295e2788ece4c554a71
Paul Andrieux <paul@appventus.com>
2016-11-30 11:18:10 +0100

    remove annotation logic for BusinessEntity

--- Bundle/CoreBundle/Cache/Builder/CacheBuilder.php
+++ Bundle/CoreBundle/Cache/Builder/CacheBuilder.php
@@ -18,40 +18,19 @@ class CacheBuilder
         $this->cache = $cache;
     }
 
-    /**
-     * save BusinessEntity.
-     */
-    public function saveBusinessEntity(BusinessEntity $businessEntity)
-    {
-        $businessEntities = $this->cache->get(BusinessEntity::CACHE_CLASSES);
-        $businessEntities[$businessEntity->getClass()] = $businessEntity;
-        $this->cache->save(BusinessEntity::CACHE_CLASSES, $businessEntities);
-    }
 
     /**
      * save Widget.
      */
     public function saveWidgetReceiverProperties($widgetName, $receiverProperties)
     {
-        $widgets = $this->cache->get(BusinessEntity::CACHE_WIDGETS, []);
+        $widgets = $this->cache->get('victoire_business_entity_widgets', []);
         if (!array_key_exists($widgetName, $widgets)) {
             $widgets[$widgetName] = [];
         }
 
         $widgets[$widgetName]['receiverProperties'] = $receiverProperties;
-        $this->cache->save(BusinessEntity::CACHE_WIDGETS, $widgets);
+        $this->cache->save('victoire_business_entity_widgets', $widgets);
     }
 
-    /**
-     * add a BusinessEntity For Widget.
-     */
-    public function addWidgetBusinessEntity($widgetName, $businessEntity)
-    {
-        $widgets = $this->cache->get(BusinessEntity::CACHE_WIDGETS, []);
-        if (!array_key_exists($widgetName, $widgets)) {
-            $widgets[$widgetName] = ['businessEntities' => []];
-        }
-        $widgets[$widgetName]['businessEntities'][$businessEntity->getId()] = $businessEntity;
-        $this->cache->save(BusinessEntity::CACHE_WIDGETS, $widgets);
-    }
 }
