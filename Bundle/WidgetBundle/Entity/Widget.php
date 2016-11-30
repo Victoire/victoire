b9c37a01431e13288b89c5ddb31abb32e8b581bb
Paul Andrieux <paul@appventus.com>
2016-11-30 15:39:37 +0100

    activate good tabs in widget forms

--- Bundle/WidgetBundle/Entity/Widget.php
+++ Bundle/WidgetBundle/Entity/Widget.php
@@ -169,15 +169,17 @@ class Widget extends BaseWidget implements VictoireQueryInterface
     }
 
     /**
-     * get the attached business entity name
+     * Get businessEntityName.
+     *
+     * @return int
      */
     public function getBusinessEntityName()
     {
-        if ($this->entityProxy && $this->entityProxy->getBusinessEntity()) {
-            return $this->entityProxy->getBusinessEntity()->getName();
+        if ($entityProxy = $this->getEntityProxy()) {
+            return $entityProxy->getBusinessEntity()->getName();
         }
 
-        return null;
+        return $this->businessEntityName;
     }
 
     /**
@@ -423,7 +425,7 @@ class Widget extends BaseWidget implements VictoireQueryInterface
             $entityProxy = $this->getEntityProxy();
 
             //if there is a proxy
-            if ($entityProxy !== null && $this->getBusinessEntityName()) {
+            if ($entityProxy !== null) {
                 $this->entity = $entityProxy->getEntity();
             }
         }
