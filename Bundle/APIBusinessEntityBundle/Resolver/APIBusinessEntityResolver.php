e4c10adbb9d33d07fa444dcb6d2c990be5a4ec49
Paul Andrieux <paul@appventus.com>
2016-12-13 10:35:51 +0100

    api can now fetch a businessEntity and the list of businessEntities

--- Bundle/APIBusinessEntityBundle/Resolver/APIBusinessEntityResolver.php
+++ Bundle/APIBusinessEntityBundle/Resolver/APIBusinessEntityResolver.php
@@ -2,7 +2,6 @@
 
 namespace Victoire\Bundle\APIBusinessEntityBundle\Resolver;
 
-use Doctrine\ORM\EntityManager;
 use Victoire\Bundle\APIBusinessEntityBundle\Entity\APIBusinessEntity;
 use Victoire\Bundle\BusinessEntityBundle\Resolver\BusinessEntityResolverInterface;
 use Victoire\Bundle\CoreBundle\Entity\EntityProxy;
@@ -17,9 +16,22 @@ class APIBusinessEntityResolver implements BusinessEntityResolverInterface
     {
         /** @var APIBusinessEntity $businessEntity */
         $businessEntity = $entityProxy->getBusinessEntity();
+        $path = sprintf("%s/%s/%s", $businessEntity->getEndpoint()->getHost(), $businessEntity->getResource(), $entityProxy->getRessourceId());
+
+        return $this->callApi($path);
+    }
+    public function getBusinessEntities(APIBusinessEntity $businessEntity)
+    {
+        $path = sprintf("%s/%s", $businessEntity->getEndpoint()->getHost(), $businessEntity->getResource());
+
+        return $this->callApi($path);
+    }
+
+    protected function callApi($path)
+    {
         $curl = curl_init();
 
-        curl_setopt($curl, CURLOPT_URL, sprintf("%s/%s/%s", $businessEntity->getEndpoint()->getHost(), $businessEntity->getResource(), $entityProxy->getRessourceId()));
+        curl_setopt($curl, CURLOPT_URL,$path);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 
         $result = curl_exec($curl);
@@ -27,5 +39,6 @@ class APIBusinessEntityResolver implements BusinessEntityResolverInterface
         curl_close($curl);
 
         return json_decode($result);
+
     }
 }
