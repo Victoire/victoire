d3ec6b4db024762f9e480a8aeb396955422993e4
Paul Andrieux <paul@appventus.com>
2016-12-26 18:27:57 +0100

    create a select2 form type

---
+++ Bundle/CoreBundle/Form/Field/APISelect2Type.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Victoire\Bundle\CoreBundle\Form\Field;
+
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+class APISelect2Type extends AbstractType
+{
+    public function getParent()
+    {
+        return ChoiceType::class;
+    }
+}
