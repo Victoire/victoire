cc1644ac43742c1e24b3b3452180090d75f0a22e
Paul Andrieux <paul@appventus.com>
2016-12-26 18:30:45 +0100

    declare a controler action to fetch an api

--- Bundle/CoreBundle/Route/RouteLoader.php
+++ Bundle/CoreBundle/Route/RouteLoader.php
@@ -66,6 +66,7 @@ class RouteLoader extends Loader
             'VictoireConfigBundle',
             'VictoireWidgetBundle',
             'VictoireSitemapBundle',
+            'VictoireAPIBusinessEntityBundle',
         ];
 
         foreach ($bundles as $bundle) {
