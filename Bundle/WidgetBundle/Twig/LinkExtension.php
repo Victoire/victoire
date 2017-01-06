cc7677e88c46a2485b6364e626d568b508062ad2
Paul Andrieux <paul@appventus.com>
2017-01-06 14:49:45 +0100

    guessBestPattern now uses _businessEntity to find relative business entity instead of class metadatas

--- Bundle/WidgetBundle/Twig/LinkExtension.php
+++ Bundle/WidgetBundle/Twig/LinkExtension.php
@@ -266,7 +266,7 @@ class LinkExtension extends \Twig_Extension
         }
         if (!$templateId) {
             $templateId = $this->BusinessPageHelper
-                ->guessBestPatternIdForEntity(new \ReflectionClass($businessEntityInstance), $businessEntityInstance->getId(), $this->em);
+                ->guessBestPatternIdForEntity($businessEntityInstance, $this->em);
         }
 
         $page = $this->pageHelper->findPageByParameters([
