742e4691d343cbf0a284811ce787f4b5777c65f8
Paul Andrieux <paul@appventus.com>
2017-01-06 18:00:46 +0100

    update docker doc and fix some config

--- .docker/php-fpm/build.sh
+++ .docker/php-fpm/build.sh
@@ -2,7 +2,7 @@ cd /var/www/victoire \
     && mkdir -p /tmp/Victoire/cache/ /tmp/Victoire/logs/ \
     && chmod -R 777 /tmp/Victoire/cache/ /tmp/Victoire/logs/ \
     && php Tests/Functionnal/bin/console --env=docker cache:warmup \
-    && php Tests/Functionnal/bin/console do:sc:up --force -e test \
+    && php Tests/Functionnal/bin/console do:sc:up --force -e docker\
     && php Tests/Functionnal/bin/console --env=docker do:fi:lo --fixtures=Tests/Functionnal/src/Acme/AppBundle/DataFixtures/seeds/ORM -n \
     && php Tests/Functionnal/bin/console --env=docker victoire:generate:view \
     && php Tests/Functionnal/bin/console --env=docker assets:install Tests/Functionnal/web \
