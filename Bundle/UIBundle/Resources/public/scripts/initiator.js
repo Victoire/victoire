69b796fd78e89bad95e6b966f8a9d049e33f5c55
Loïc Goyet <loic@appventus.com>
2017-01-11 11:59:34 +0100

    list all the required assets to import for the new styleguide

---
+++ Bundle/UIBundle/Resources/public/scripts/initiator.js
@@ -0,0 +1,4 @@
+var mdFormGroups = document.querySelectorAll('[data-flag="mdf-group"]');
+new MDFormGroups(mdFormGroups);
+
+var drops = new TrowelDrops(document.querySelectorAll('[data-flag="drops"]'));
