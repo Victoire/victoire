15cf59782e47bf1d7154a7f8fccb71a47085e5cc
Alexis Lefebvre <alexis@troopers.email>
2017-01-12 18:14:59 +0100

    Entity proxy: Filter by available translations

--- Bundle/CoreBundle/Form/EntityProxyFormType.php
+++ Bundle/CoreBundle/Form/EntityProxyFormType.php
@@ -2,10 +2,14 @@
 
 namespace Victoire\Bundle\CoreBundle\Form;
 
+use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Victoire\Bundle\CoreBundle\Helper\CurrentViewHelper;
+use Knp\DoctrineBehaviors\Model\Translatable\Translatable;
 
 /**
  * Create an entity proxy for the widget.
@@ -14,6 +18,18 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
  */
 class EntityProxyFormType extends AbstractType
 {
+    /** @var RequestStack */
+    private $requestStack;
+
+    /**
+     * EntityProxyFormType constructor.
+     * @param RequestStack $requestStack
+     */
+    public function __construct(RequestStack $requestStack)
+    {
+        $this->requestStack = $requestStack;
+    }
+
     /**
      * define form fields.
      *
@@ -22,6 +38,8 @@ class EntityProxyFormType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $locale = $this->requestStack->getCurrentRequest()->getLocale();
+
         //add the link to the business entity instance
         //it depends of the form
         $builder
@@ -33,6 +51,17 @@ class EntityProxyFormType extends AbstractType
                 'attr'        => [
                     'class' => 'add_'.$options['business_entity_id'].'_link picker_entity_select',
                 ],
+                'query_builder' => function (EntityRepository $er) use ($options, $locale) {
+                    // Don't display entities that don't have translations in the current locale.
+                    if (in_array(Translatable::class, class_uses($options['namespace']))) {
+                        return $er->createQueryBuilder('entity')
+                            ->join('entity.translations', 't')
+                            ->andWhere('t.locale = :s')
+                            ->setParameter(':s', $locale);
+                    }
+
+                    return $er->createQueryBuilder('entity');
+                },
             ]);
     }
 
