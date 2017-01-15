89e5c65d12b79886d7e4c63722f86c07df888d41
Loïc Goyet <loic@appventus.com>
2017-01-15 15:24:55 +0100

    collapses components

--- Bundle/UIBundle/Resources/public/scripts/initiator.js
+++ Bundle/UIBundle/Resources/public/scripts/initiator.js
@@ -1,3 +1,4 @@
 var mdForm = new MDFormGroups(document.querySelectorAll('[data-flag="v-mdForm"]'));
 var drops = new TrowelDrops(document.querySelectorAll('[data-flag="v-drop"]'));
 var modals = new VTrowelModals(document.querySelectorAll('[data-flag="v-modal"]'))
+var collapses = new TrowelCollapses(document.querySelectorAll('[data-flag="v-collapse"]'));
