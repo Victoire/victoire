ec215f167ff5273002e435404e52bd2d76c68a4b
Grégoire Humeau <gregoire@appventus.com>
2017-01-30 17:49:37 +0800

    Change AssociatedEntityToWarm class to handle also OneToMany associations

--- Bundle/WidgetMapBundle/Warmer/AssociatedEntityToWarm.php
+++ Bundle/WidgetMapBundle/Warmer/AssociatedEntityToWarm.php
@@ -4,22 +4,57 @@ namespace Victoire\Bundle\WidgetMapBundle\Warmer;
 
 class AssociatedEntityToWarm
 {
+
+    const TYPE_MANY_TO_ONE = 'many_to_one';
+    const TYPE_ONE_TO_MANY = 'one_to_many';
+
+    protected $type;
     protected $inheritorEntity;
     protected $inheritorPropertyName;
     protected $entityId;
+    protected $mappedBy;
 
     /**
      * Constructor.
      *
+     * @param null $type
      * @param null $inheritorEntity
      * @param null $inheritorPropertyName
-     * @param null $entityId              for ManyToOne type
+     * @param null $entityId
+     * @param null $mappedBy for OneToMany type only
      */
-    public function __construct($inheritorEntity = null, $inheritorPropertyName = null, $entityId = null)
+    public function __construct(
+        $type = null,
+        $inheritorEntity = null,
+        $inheritorPropertyName = null,
+        $entityId = null,
+        $mappedBy = null)
     {
+        $this->type = $type;
         $this->inheritorEntity = $inheritorEntity;
         $this->inheritorPropertyName = $inheritorPropertyName;
         $this->entityId = $entityId;
+        $this->mappedBy = $mappedBy;
+    }
+
+    /**
+     * @return null
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * @param null $type
+     *
+     * @return $this
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
     }
 
     /**
@@ -80,6 +115,26 @@ class AssociatedEntityToWarm
     public function setEntityId($entityId)
     {
         $this->entityId = $entityId;
+
+        return $this;
+    }
+
+    /**
+     * @return null
+     */
+    public function getMappedBy()
+    {
+        return $this->mappedBy;
+    }
+
+    /**
+     * @param null $mappedBy
+     *
+     * @return $this
+     */
+    public function setMappedBy($mappedBy)
+    {
+        $this->mappedBy = $mappedBy;
 
         return $this;
     }
