f7692d8cbd41c3870da58322bdf60a0a1c9f778e
Loïc Goyet <loic@appventus.com>
2017-02-20 10:48:30 +0100

    :construction: WIP

--- Bundle/BusinessEntityBundle/Resources/public/js/businessProperty.js
+++ Bundle/BusinessEntityBundle/Resources/public/js/businessProperty.js
@@ -1,28 +1,10 @@
 /*global $vic*/
 
-var businessProperty = {};
-
-businessProperty.initPropertyHelper = function ()
-{
-    "use strict";
-    businessProperty.currentInput = null;
-    
-    //if an input or a textarea lose the focus
-    $vic("form").find('input, textarea').focusout(function () {
-        //we memorize the input
-        businessProperty.currentInput = $vic(this);
-    });
-};
-
-
 //we add the {{ item.XXX }} string to the current input
-businessProperty.addPropertyToCurrentInput = function (businessPropertyId)
-{
-    "use strict";
-    
-    //get the current input
-    var currentInput = businessProperty.currentInput;
-    
+var addPropertyToCurrentInput = function (button, businessPropertyId) {
+    var currentInput = $vic(button).closest('.v-form-group').find('input');
+    var label = $vic(button).closest('.v-form-group').find('.v-form-group__label');
+
     //there was an input
     if (currentInput !== null) {
         //the string to insert
@@ -33,5 +15,8 @@ businessProperty.addPropertyToCurrentInput = function (businessPropertyId)
         var newString = currentString + insertString;        
         //set the new string
         currentInput.val(newString);
+
+        // unfold label
+        label.attr('data-mdform', 'unfolded');
     }
 };
