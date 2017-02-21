55491279812127447eb95c347ef782d51afade50
Paul Andrieux <paul@appventus.com>
2017-02-21 10:43:52 +0100

    use doctrine migrations to upgrade database structure

--- Bundle/CoreBundle/DoctrineMigrations/Version20160510154806.php
+++ Bundle/CoreBundle/DoctrineMigrations/Version20160510154806.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Application\Migrations;
+namespace Victoire\Migrations;
 
 use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
@@ -17,6 +17,7 @@ class Version20160510154806 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->skipIf(!empty($this->connection->fetchAll('SHOW TABLES LIKE "vic_view_translations"')), 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('RENAME TABLE `vic_view_translations` TO `vic_view_translations_legacy`');
     }
@@ -28,6 +29,7 @@ class Version20160510154806 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->skipIf(!empty($this->connection->fetchAll('SHOW TABLES LIKE "vic_view_translations_legacy"')), 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('RENAME TABLE `vic_view_translations_legacy` TO `vic_view_translations`');
     }
