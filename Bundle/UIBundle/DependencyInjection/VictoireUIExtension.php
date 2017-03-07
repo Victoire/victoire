2cef43ff94e52cfef63640a0774ac64872474143
Grégoire Humeau <gregoire@appventus.com>
2017-03-07 15:46:23 +0100

    Remove useless use in VictoireUiExtension

--- Bundle/UIBundle/DependencyInjection/VictoireUIExtension.php
+++ Bundle/UIBundle/DependencyInjection/VictoireUIExtension.php
@@ -2,9 +2,7 @@
 
 namespace Victoire\Bundle\UIBundle\DependencyInjection;
 
-use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class VictoireUIExtension extends Extension
