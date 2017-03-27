65b118a1546403cf56725b967d8549d1c00bce22
Paul Andrieux <paul@troopers.email>
2017-03-27 12:16:20 +0200

    CS fixes

--- Bundle/CoreBundle/Form/EntityProxyFormType.php
+++ Bundle/CoreBundle/Form/EntityProxyFormType.php
@@ -8,7 +8,6 @@ use Knp\DoctrineBehaviors\Model\Translatable\Translatable;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\CallbackTransformer;
-use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\HttpFoundation\RequestStack;
--- Bundle/CoreBundle/Form/Field/BusinessEntityHiddenType.php
+++ Bundle/CoreBundle/Form/Field/BusinessEntityHiddenType.php
@@ -6,25 +6,25 @@ use Doctrine\ORM\EntityManager;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\CallbackTransformer;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class BusinessEntityHiddenType extends AbstractType
 {
-
     protected $entityManager;
 
     public function __construct(EntityManager $entityManager)
     {
         $this->entityManager = $entityManager;
-
     }
-    public function getParent() {
+
+    public function getParent()
+    {
         return HiddenType::class;
     }
 
-    public function buildForm(FormBuilderInterface $builder, array $options) {
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
         $entityManager = $this->entityManager;
         parent::buildForm($builder, $options);
 
@@ -55,5 +55,4 @@ class BusinessEntityHiddenType extends AbstractType
             'translation_domain' => 'victoire',
         ]);
     }
-
 }
