a262fd7d92ca10c501ab0e98081b413672d42a2e
Grégoire Humeau <gregoire@appventus.com>
2017-03-31 17:40:36 +0200

    Remove TestsBundleKernelAdderCommand and replace it with new AppKernel method

--- Tests/App/app/AppKernel.php
+++ Tests/App/app/AppKernel.php
@@ -12,66 +12,66 @@ class AppKernel extends Kernel
      */
     public function registerBundles()
     {
-        return [
-            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
-            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
-            new Symfony\Bundle\TwigBundle\TwigBundle(),
-            new Symfony\Bundle\MonologBundle\MonologBundle(),
-            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
-            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
-            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
-            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
+        return array_merge(
+            [
+                new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+                new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+                new Symfony\Bundle\TwigBundle\TwigBundle(),
+                new Symfony\Bundle\MonologBundle\MonologBundle(),
+                new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+                new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+                new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+                new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+                new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
 
-            new A2lix\TranslationFormBundle\A2lixTranslationFormBundle(),
-            new Bazinga\Bundle\JsTranslationBundle\BazingaJsTranslationBundle(),
-            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
-            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
-            new FOS\UserBundle\FOSUserBundle(),
-            new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
-            new JMS\SerializerBundle\JMSSerializerBundle(),
-            new Knp\DoctrineBehaviors\Bundle\DoctrineBehaviorsBundle(),
-            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
-            new Liip\ImagineBundle\LiipImagineBundle(),
-            new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle(),
-            new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle(),
-            new Snc\RedisBundle\SncRedisBundle(),
-            new Troopers\AsseticInjectorBundle\TroopersAsseticInjectorBundle(),
-            new Troopers\AlertifyBundle\TroopersAlertifyBundle(),
+                new A2lix\TranslationFormBundle\A2lixTranslationFormBundle(),
+                new Bazinga\Bundle\JsTranslationBundle\BazingaJsTranslationBundle(),
+                new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+                new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
+                new FOS\UserBundle\FOSUserBundle(),
+                new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
+                new JMS\SerializerBundle\JMSSerializerBundle(),
+                new Knp\DoctrineBehaviors\Bundle\DoctrineBehaviorsBundle(),
+                new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+                new Liip\ImagineBundle\LiipImagineBundle(),
+                new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle(),
+                new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle(),
+                new Snc\RedisBundle\SncRedisBundle(),
+                new Troopers\AsseticInjectorBundle\TroopersAsseticInjectorBundle(),
+                new Troopers\AlertifyBundle\TroopersAlertifyBundle(),
 
-            //Victoire bundles
-            new Victoire\Bundle\AnalyticsBundle\VictoireAnalyticsBundle(),
-            new Victoire\Bundle\CoreBundle\VictoireCoreBundle(),
-            new Victoire\Bundle\CriteriaBundle\VictoireCriteriaBundle(),
-            new Victoire\Bundle\BlogBundle\VictoireBlogBundle(),
-            new Victoire\Bundle\BusinessEntityBundle\VictoireBusinessEntityBundle(),
-            new Victoire\Bundle\BusinessPageBundle\VictoireBusinessPageBundle(),
-            new Victoire\Bundle\FilterBundle\VictoireFilterBundle(),
-            new Victoire\Bundle\I18nBundle\VictoireI18nBundle(),
-            new Victoire\Bundle\FormBundle\VictoireFormBundle(),
-            new Victoire\Bundle\PageBundle\VictoirePageBundle(),
-            new Victoire\Bundle\QueryBundle\VictoireQueryBundle(),
-            new Victoire\Bundle\MediaBundle\VictoireMediaBundle(),
-            new Victoire\Bundle\SeoBundle\VictoireSeoBundle(),
-            new Victoire\Bundle\SitemapBundle\VictoireSitemapBundle(),
-            new Victoire\Bundle\TemplateBundle\VictoireTemplateBundle(),
-            new Victoire\Bundle\TwigBundle\VictoireTwigBundle(),
-            new Victoire\Bundle\UIBundle\VictoireUIBundle(),
-            new Victoire\Bundle\UserBundle\VictoireUserBundle(),
-            new Victoire\Bundle\ViewReferenceBundle\ViewReferenceBundle(),
-            new Victoire\Bundle\WidgetBundle\VictoireWidgetBundle(),
-            new Victoire\Bundle\WidgetMapBundle\VictoireWidgetMapBundle(),
+                //Victoire bundles
+                new Victoire\Bundle\AnalyticsBundle\VictoireAnalyticsBundle(),
+                new Victoire\Bundle\CoreBundle\VictoireCoreBundle(),
+                new Victoire\Bundle\CriteriaBundle\VictoireCriteriaBundle(),
+                new Victoire\Bundle\BlogBundle\VictoireBlogBundle(),
+                new Victoire\Bundle\BusinessEntityBundle\VictoireBusinessEntityBundle(),
+                new Victoire\Bundle\BusinessPageBundle\VictoireBusinessPageBundle(),
+                new Victoire\Bundle\FilterBundle\VictoireFilterBundle(),
+                new Victoire\Bundle\I18nBundle\VictoireI18nBundle(),
+                new Victoire\Bundle\FormBundle\VictoireFormBundle(),
+                new Victoire\Bundle\PageBundle\VictoirePageBundle(),
+                new Victoire\Bundle\QueryBundle\VictoireQueryBundle(),
+                new Victoire\Bundle\MediaBundle\VictoireMediaBundle(),
+                new Victoire\Bundle\SeoBundle\VictoireSeoBundle(),
+                new Victoire\Bundle\SitemapBundle\VictoireSitemapBundle(),
+                new Victoire\Bundle\TemplateBundle\VictoireTemplateBundle(),
+                new Victoire\Bundle\TwigBundle\VictoireTwigBundle(),
+                new Victoire\Bundle\UIBundle\VictoireUIBundle(),
+                new Victoire\Bundle\UserBundle\VictoireUserBundle(),
+                new Victoire\Bundle\ViewReferenceBundle\ViewReferenceBundle(),
+                new Victoire\Bundle\WidgetBundle\VictoireWidgetBundle(),
+                new Victoire\Bundle\WidgetMapBundle\VictoireWidgetMapBundle(),
 
-            //Victoire minmal test bundles
-            new Victoire\Widget\ForceBundle\VictoireWidgetForceBundle(),
-            new Victoire\Widget\LightSaberBundle\VictoireWidgetLightSaberBundle(),
-            new Victoire\Widget\ButtonBundle\VictoireWidgetButtonBundle(),
-            new Victoire\Widget\TextBundle\VictoireWidgetTextBundle(),
-            new Acme\AppBundle\AcmeAppBundle(),
-
-            //Here are added Victoire Bundles that need to be tested
-            //TestBundlesKernelAdderSpot
-        ];
+                //Victoire minmal test bundles
+                new Victoire\Widget\ForceBundle\VictoireWidgetForceBundle(),
+                new Victoire\Widget\LightSaberBundle\VictoireWidgetLightSaberBundle(),
+                new Victoire\Widget\ButtonBundle\VictoireWidgetButtonBundle(),
+                new Victoire\Widget\TextBundle\VictoireWidgetTextBundle(),
+                new Acme\AppBundle\AcmeAppBundle(),
+            ],
+            $this->getTestBundles()
+        );
     }
 
     /**
@@ -96,5 +96,33 @@ class AppKernel extends Kernel
     public function getLogDir()
     {
         return sys_get_temp_dir().'/Victoire/logs/'.$this->environment;
+    }
+
+    /**
+     * Include Tests Bundles defined in Bundles.php file.
+     *
+     * @return array
+     *
+     * @throws ErrorException
+     */
+    private function getTestBundles()
+    {
+        $bundlesFile = __DIR__.'/../../../../../../Tests/Bundles.php';
+
+        if(file_exists($bundlesFile)) {
+            include($bundlesFile);
+
+            if(!isset($victoireTestBundles)) {
+                throw new \ErrorException(sprintf('$victoireTestBundles is not defined in Bundles.php file'));
+            }
+
+            if(!is_array($victoireTestBundles)) {
+                throw new \ErrorException(sprintf('$victoireTestBundles defined in Bundles.php must be an array'));
+            }
+
+            return $victoireTestBundles;
+        }
+
+        return [];
     }
 }
