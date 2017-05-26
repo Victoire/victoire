688eb7e82dced733c497a962dd1c65ac29afb445
Paul Andrieux <paul@troopers.email>
2017-05-26 13:04:38 +0200

    disable debug in ci no debug env

--- Tests/App/web/app_ci.php
+++ Tests/App/web/app_ci.php
@@ -4,7 +4,6 @@ use Symfony\Component\Debug\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
 require_once __DIR__.'/../app/AppKernel.php';
-Debug::enable();
 $kernel = new AppKernel('ci', false);
 $kernel->loadClassCache();
 Request::enableHttpMethodParameterOverride();
