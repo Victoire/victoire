2cde456e53fd7ec2a43a69ed853cbeadc63f7fcc
Naomi Hauret <naomi@troopers.email>
2017-08-02 21:05:50 +0200

    added missing annotation

--- Bundle/BlogBundle/Entity/Category.php
+++ Bundle/BlogBundle/Entity/Category.php
@@ -4,6 +4,7 @@ namespace Victoire\Bundle\BlogBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Mapping\Annotation as Gedmo;
+use Victoire\Bundle\CoreBundle\Annotations as Vic;
 
 /**
  * Category.
