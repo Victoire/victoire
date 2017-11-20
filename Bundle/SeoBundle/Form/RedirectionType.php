17bdbfdacde4ff2124f682ad42fed49fbbbfc5c4
arthur@troopers.email <arthur@troopers.email>
2017-11-20 13:25:16 +0100

    add option withUrl for LinkType

--- Bundle/SeoBundle/Form/RedirectionType.php
+++ Bundle/SeoBundle/Form/RedirectionType.php
@@ -3,10 +3,11 @@
 namespace Victoire\Bundle\SeoBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Victoire\Bundle\FormBundle\Form\Type\LinkType;
+use Victoire\Bundle\SeoBundle\Entity\Redirection;
 
 /**
  * Class RedirectionType.
@@ -15,29 +16,25 @@ class RedirectionType extends AbstractType
 {
     /**
      * @param FormBuilderInterface $builder
-     * @param array $options
+     * @param array                $options
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder
-            ->add('input', TextType::class, [
+        if (true === $options['withUrl']) {
+            $builder->add('url', UrlType::class, [
                 'label' => false,
-                'required' => true,
                 'attr' => [
-                    'class' => 'vic-form-control'
+                    'placeholder' => 'redirection.form.url.placeholder',
+                    'class' => 'vic-form-control',
                 ]
-            ])
-            ->add('output', LinkType::class, [
-//                'refresh-target' => '#basics',
-//                'label'          => 'form.pageSeo.redirectTo.label',
-//                'vic_help_block' => 'form.pageSeo.redirectTo.vic_help_block',
-                'label' => false,
-                'required' => false
-//                'attr' => [
-//                    'class' => 'vic-form-control'
-//                ]
-            ])
-        ;
+            ]);
+        }
+
+        $builder->add('link', LinkType::class, [
+            'label' => false,
+            'withTarget' => false,
+            'refresh-target' => $options['containerId']
+        ]);
     }
 
     /**
@@ -45,13 +42,12 @@ class RedirectionType extends AbstractType
      */
     public function configureOptions(OptionsResolver $resolver)
     {
+        $resolver->setRequired(['containerId']);
         $resolver->setDefaults([
             'method'             => 'POST',
-            'data_class'         => 'Victoire\Bundle\SeoBundle\Entity\Redirection',
             'translation_domain' => 'victoire',
-            'attr' => [
-                'ic-target' => '#vic-modal-container'
-            ]
+            'data_class'         => Redirection::class,
+            'withUrl'            => false
         ]);
     }
 
