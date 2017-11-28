f26e55a73a83283b9458b062c264ed20667d5fc5
Baptiste <baptiste@troopers.email>
2017-11-28 11:06:11 +0100

    apply ci fixes

--- Bundle/CoreBundle/Entity/View.php
+++ Bundle/CoreBundle/Entity/View.php
@@ -240,7 +240,7 @@ abstract class View
      *
      * @param View $parent
      */
-    public function setParent(View $parent = null)
+    public function setParent(self $parent = null)
     {
         $this->parent = $parent;
         if ($parent && !($this instanceof VirtualBusinessPage)) {
@@ -319,7 +319,7 @@ abstract class View
      *
      * @param View $child
      */
-    public function addChild(View $child)
+    public function addChild(self $child)
     {
         $this->children[] = $child;
     }
@@ -329,7 +329,7 @@ abstract class View
      *
      * @param View $child
      */
-    public function removeChild(View $child)
+    public function removeChild(self $child)
     {
         $this->children->removeElement($child);
     }
@@ -748,7 +748,7 @@ abstract class View
      *
      * @return bool
      */
-    public function isTemplateOf(View $view)
+    public function isTemplateOf(self $view)
     {
         while ($_view = $view->getTemplate()) {
             if ($this === $_view) {
