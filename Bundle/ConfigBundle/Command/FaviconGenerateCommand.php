ff5fa9d60b83d3c71b866ba95845c87ce5837e03
Leny Bernard <leny@troopers.email>
2018-04-19 16:11:47 -0400

    add Favicon Generator to generate favicons with real-favicon

---
+++ Bundle/ConfigBundle/Command/FaviconGenerateCommand.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Victoire\Bundle\ConfigBundle\Command;
+
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Mapping\Entity;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
+use Victoire\Bundle\ConfigBundle\Entity\GlobalConfig;
+use Victoire\Bundle\ConfigBundle\Favicon\FaviconGenerator;
+use Victoire\Bundle\ConfigBundle\Favicon\GenerateFaviconException;
+
+class FaviconGenerateCommand extends ContainerAwareCommand
+{
+    /** @var EntityManager $entityManager */
+    private $entityManager;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configure()
+    {
+        parent::configure();
+
+        $this
+            ->setName('victoire:favicons:generate')
+            ->addArgument('target', InputArgument::OPTIONAL, 'The target directory', 'web')
+            ->addArgument(
+                'configPath',
+                InputArgument::OPTIONAL,
+                'The target directory',
+                'faviconConfig.json'
+            )
+            ->addOption('realFaviconPath',
+                    'real',
+                    InputArgument::OPTIONAL,
+                    'The cli-real-favicon generator full path',
+                    'vendor/victoire/victoire/Bundle/UIBundle/Resources/config/node_modules/cli-real-favicon/real-favicon.js'
+            )
+            ->setDescription('Generate icons according to global config');
+    }
+
+    /**
+     * Generate favicons thanks to realfavicon generator.
+     *
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     *
+     * @throws \Exception
+     *
+     * @return bool
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        /** @var EntityManager $entityManager */
+        $entityManager = $this->getContainer()->get('doctrine.orm.entity_manager');
+        try {
+            $output->writeln('<fg=white;bg=cyan;options=bold>Generating Favicon</>');
+            $generator = $this->getContainer()->get(FaviconGenerator::class);
+            $files = $generator->generate(
+                $entityManager->getRepository(GlobalConfig::class)->find(1)
+            );
+            $output->writeln(sprintf('<comment>%s</comment><info>%s</info>', 'Command line : ', $generator->process->getCommandLine()));
+            $output->writeln(sprintf('<info>%s</info>', $generator->process->getOutput()));
+            $output->writeln(sprintf('<fg=green>Ok</> The favicons have been generated in <info>%s</info> directory', $generator->target));
+            foreach ($files as $file) {
+                $output->writeln(sprintf('<comment>- %s</comment>', $file));
+            }
+        } catch (ProcessFailedException $e) {
+            $output->writeln(sprintf('<error>%s</error>', $e->getMessage()));
+        }
+    }
+}
---
+++ Bundle/ConfigBundle/Favicon/FaviconGenerator.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Victoire\Bundle\ConfigBundle\Favicon;
+
+use Doctrine\ORM\EntityManager;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Filesystem\Exception\FileNotFoundException;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
+use Victoire\Bundle\ConfigBundle\Entity\GlobalConfig;
+
+class FaviconGenerator
+{
+    /**
+     * @var FaviconConfigDumper
+     */
+    private $faviconConfigDumper;
+    /**
+     * @var string
+     */
+    private $realFaviconPath;
+    /**
+     * @var string
+     */
+    public $target;
+
+    /**
+     * FaviconGenerator constructor.
+     * @param FaviconConfigDumper $faviconConfigDumper
+     * @param string $realFaviconPath
+     * @param string $target
+     * @param string $cwd current working directory for process
+     */
+    public function __construct(FaviconConfigDumper $faviconConfigDumper, string $realFaviconPath, string $target, string $cwd)
+    {
+        $this->faviconConfigDumper = $faviconConfigDumper;
+        $this->realFaviconPath = $realFaviconPath;
+        $this->target = $target;
+        $this->cwd = $cwd;
+    }
+
+    /**
+     * Generate favicons from realfavicon generator and a GlobalConfig object.
+     *
+     * @return array The generated files
+     */
+    public function generate(GlobalConfig $globalConfig, $path = 'faviconConfig.json')
+    {
+        return $this->generateFromConfigFile($this->faviconConfigDumper->dump($globalConfig, $path));
+    }
+
+    /**
+     * Generate favicons from a config file.
+     *
+     * @return array The generated files
+     */
+    public function generateFromConfigFile(string $configPath)
+    {
+        try {
+            $fileSystem = new Filesystem();
+            if (!$fileSystem->exists($configPath)) {
+                throw new FileNotFoundException(sprintf('The file %s does not exist.', $configPath));
+            }
+
+            $faviconRequestPath = $this->cwd.'/faviconRequest.json';
+            $fileSystem = new Filesystem();
+            if (!$fileSystem->exists($faviconRequestPath)) {
+                $fileSystem->copy($configPath, $faviconRequestPath);
+            }
+            $this->process = new Process(sprintf(
+                '%s generate %s %s %s',
+                $this->realFaviconPath,
+                $configPath,
+                $faviconRequestPath,
+                $this->target
+            ), $this->cwd);
+            $this->process->mustRun();
+
+            $response = json_decode(file_get_contents($faviconRequestPath));
+            if ($response->result->status === 'success') {
+                $fileSystem->remove($faviconRequestPath);
+                $files = [];
+                foreach ($response->favicon->files_urls as $file) {
+                    $path = parse_url($file, PHP_URL_PATH);
+                    $pathFragments = explode('/', $path);
+                    $files[] = sprintf('%s/%s', $this->target, end($pathFragments));
+                }
+
+                return $files;
+            } else {
+                throw new GenerateFaviconException(sprintf(
+                        'An issue occured with the real favicon generation, you can dive into the %s file',
+                        $faviconRequestPath
+                    )
+                );
+            }
+        } catch (FileNotFoundException | ProcessFailedException $e) {
+            throw $e;
+        }
+    }
+}
