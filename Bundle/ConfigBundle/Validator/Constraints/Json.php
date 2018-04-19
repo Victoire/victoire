15bc8b407199f6ea02fd9d17727105e80f96683d
Leny Bernard <leny@troopers.email>
2018-04-19 15:48:17 -0400

    add json validator and SemanticalOrganizationJsonLD validator

---
+++ Bundle/ConfigBundle/Validator/Constraints/Json.php
@@ -0,0 +1,16 @@
+<?php
+namespace Victoire\Bundle\ConfigBundle\Validator\Constraints;
+
+use Symfony\Component\Validator\Constraint;
+use Victoire\Bundle\ConfigBundle\Validator\JsonValidator;
+
+/**
+ * @Annotation
+ */
+class Json extends Constraint
+{
+    public function validatedBy()
+    {
+        return JsonValidator::class;
+    }
+}
