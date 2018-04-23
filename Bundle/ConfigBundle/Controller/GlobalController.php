e4b02ff6187c10460eaea2f938f9800fd37e5142
Leny Bernard <leny@troopers.email>
2018-04-24 01:28:30 +0200

    avoid strange condition

--- Bundle/ConfigBundle/Controller/GlobalController.php
+++ Bundle/ConfigBundle/Controller/GlobalController.php
@@ -40,30 +40,33 @@ class GlobalController extends Controller
         $initialLogo = $globalConfig->getLogo();
         $form->handleRequest($request);
 
-        if (($submitted = $form->isSubmitted()) && $form->isValid()) {
-            //cloning the entity will give it a new id in order to invalidate browser cache (in meta.html.twig)
-            $entityManager->clear(GlobalConfig::class);
-            $entityManager->persist($globalConfig);
-            $entityManager->flush();
-            if ($initialLogo !== $globalConfig->getLogo() && $globalConfig->getLogo() !== null) {
-                $this->container->get(FaviconGenerator::class)->generate(
-                    $globalConfig,
-                    $this->getParameter('kernel.project_dir').'/faviconConfig.json'
-                );
-            }
-            $this->congrat('victoire.config.global.edit.success');
+        $success = true;
+        if ($form->isSubmitted()) {
+            if (true === $success = $form->isValid()) {
+                //cloning the entity will give it a new id in order to invalidate browser cache (in meta.html.twig)
+                $entityManager->clear(GlobalConfig::class);
+                $entityManager->persist($globalConfig);
+                $entityManager->flush();
+                if ($initialLogo !== $globalConfig->getLogo() && $globalConfig->getLogo() !== null) {
+                    $this->container->get(FaviconGenerator::class)->generate(
+                        $globalConfig,
+                        $this->getParameter('kernel.project_dir') . '/faviconConfig.json'
+                    );
+                }
+                $this->congrat('victoire.config.global.edit.success');
 
-            return new JsonResponse([
-                'url'     => $this->generateUrl('victoire_core_homepage_show'),
-                'success' => true,
-            ]);
+                return new JsonResponse([
+                    'url' => $this->generateUrl('victoire_core_homepage_show'),
+                    'success' => true,
+                ]);
+            }
         }
 
         return new JsonResponse([
             'html' => $this->renderView('VictoireConfigBundle:global:edit.html.twig', [
                 'form' => $form->createView(),
             ]),
-            'success' => !$submitted,
+            'success' => $success,
         ]);
     }
 }
