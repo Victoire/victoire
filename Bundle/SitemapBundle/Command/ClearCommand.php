8d8c826903c29b3ad2744812feb2bdfef4fbee32
Leny Bernard <leny@troopers.email>
2019-03-31 23:41:51 +0200

    add generate and clear victoire:sitemap:* commands

---
+++ Bundle/SitemapBundle/Command/ClearCommand.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Victoire\Bundle\SitemapBundle\Command;
+
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ClearCommand extends GenerateCommand
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this->setName('victoire:sitemap:clear')
+            ->addOption('locale', 'l', InputOption::VALUE_OPTIONAL, 'The locale you want to clear', 'all')
+            ->setDescription('Clear sitemap in cache');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln(sprintf('<info>Flusing %s...</info>', $input->getOption('locale')));
+        foreach ($this->getLocalesFromInput($input) as $locale) {
+            $output->write(sprintf('<info>♻ %s </info>', $locale));
+            $this->redis->del("sitemap.$locale");
+            $output->writeln('<info>🎉</info>');
+        }
+    }
+}
--- Bundle/SitemapBundle/Command/GenerateCommand.php
+++ Bundle/SitemapBundle/Command/GenerateCommand.php
@@ -3,16 +3,17 @@
 namespace Victoire\Bundle\SitemapBundle\Command;
 
 use Predis\Client;
-use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Victoire\Bundle\SitemapBundle\Domain\Export\SitemapExportHandler;
 
-class GenerateCommand extends ContainerAwareCommand
+class GenerateCommand extends Command
 {
-    private $redis;
-    private $handler;
-    private $locales;
+    protected $redis;
+    protected $handler;
+    protected $locales;
 
     public function __construct(Client $redis, SitemapExportHandler $handler, array $locales)
     {
@@ -28,7 +29,8 @@ class GenerateCommand extends ContainerAwareCommand
      */
     protected function configure()
     {
-        $this->setName('victoire:generate:sitemap')
+        $this->setName('victoire:sitemap:generate')
+            ->addOption('locale', 'l', InputOption::VALUE_OPTIONAL, 'The locale you want to generate', 'all')
             ->setDescription('Generate sitemap in cache');
     }
 
@@ -37,9 +39,31 @@ class GenerateCommand extends ContainerAwareCommand
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        foreach ($this->locales as $locale) {
+        $output->writeln(sprintf('<info>Generating sitemap for %s locale(s)...</info>', $input->getOption('locale')));
+        foreach ($this->getLocalesFromInput($input) as $locale) {
+            $output->write(sprintf('<info>♻ %s </info>', $locale));
             $pages = $this->handler->handle($locale);
             $this->redis->set("sitemap.$locale", $this->handler->serialize($pages));
+            $output->writeln('<info>🎉</info>');
         }
+    }
+
+    protected function getLocalesFromInput(InputInterface $input)
+    {
+        $locales = $this->locales;
+        $locale = $input->getOption('locale');
+        if ($locale != 'all') {
+            //validate the given locale is a valid locale
+            if (!array_search($locale, $locales)) {
+                throw new \Exception(sprintf(
+                        'The given locale %s doesn\'t exist (%s)',
+                        $locale,
+                        implode($locales))
+                );
+            }
+            $locales = [$locale];
+        }
+
+        return $locales;
     }
 }
