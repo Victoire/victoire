aac1644ef7bc761a5179732a62d0fb5638fa2b11
Leny BERNARD <leny@troopers.email>
2019-05-30 09:52:44 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- Bundle/CoreBundle/DependencyInjection/Compiler/OverrideServiceCompilerPass.php
+++ Bundle/CoreBundle/DependencyInjection/Compiler/OverrideServiceCompilerPass.php
@@ -9,22 +9,22 @@ class OverrideServiceCompilerPass implements CompilerPassInterface
 {
     public function process(ContainerBuilder $container)
     {
-        if($container->hasDefinition('assetic.filter.cssrewrite')) {
+        if ($container->hasDefinition('assetic.filter.cssrewrite')) {
             $container->getDefinition('assetic.filter.cssrewrite')->setPublic(true);
         }
-        if($container->hasDefinition('assetic.filter.less')) {
+        if ($container->hasDefinition('assetic.filter.less')) {
             $container->getDefinition('assetic.filter.less')->setPublic(true);
         }
-        if($container->hasDefinition('assetic.filter.scss')) {
+        if ($container->hasDefinition('assetic.filter.scss')) {
             $container->getDefinition('assetic.filter.scss')->setPublic(true);
         }
-        if($container->hasDefinition('assetic.filter_manager')) {
+        if ($container->hasDefinition('assetic.filter_manager')) {
             $container->getDefinition('assetic.filter_manager')->setPublic(true);
         }
-        if($container->hasDefinition('assetic.asset_manager')) {
+        if ($container->hasDefinition('assetic.asset_manager')) {
             $container->getDefinition('assetic.asset_manager')->setPublic(true);
         }
-        if($container->hasDefinition('victoire_core.entity_proxy.cache_driver')) {
+        if ($container->hasDefinition('victoire_core.entity_proxy.cache_driver')) {
             $container->getDefinition('victoire_core.entity_proxy.cache_driver')->setPublic(true);
         }
     }
