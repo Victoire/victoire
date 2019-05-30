3892442ed249c42c7102fa963b7b82554401436b
Leny Bernard <leny@troopers.email>
2019-05-30 11:51:58 +0200

    Make service definition only is defined

--- Bundle/CoreBundle/DependencyInjection/Compiler/OverrideServiceCompilerPass.php
+++ Bundle/CoreBundle/DependencyInjection/Compiler/OverrideServiceCompilerPass.php
@@ -9,11 +9,23 @@ class OverrideServiceCompilerPass implements CompilerPassInterface
 {
     public function process(ContainerBuilder $container)
     {
-        $container->getDefinition('assetic.filter.cssrewrite')->setPublic(true);
-        $container->getDefinition('assetic.filter.less')->setPublic(true);
-        $container->getDefinition('assetic.filter.scss')->setPublic(true);
-        $container->getDefinition('assetic.filter_manager')->setPublic(true);
-        $container->getDefinition('assetic.asset_manager')->setPublic(true);
-        $container->getDefinition('victoire_core.entity_proxy.cache_driver')->setPublic(true);
+        if($container->hasDefinition('assetic.filter.cssrewrite')) {
+            $container->getDefinition('assetic.filter.cssrewrite')->setPublic(true);
+        }
+        if($container->hasDefinition('assetic.filter.less')) {
+            $container->getDefinition('assetic.filter.less')->setPublic(true);
+        }
+        if($container->hasDefinition('assetic.filter.scss')) {
+            $container->getDefinition('assetic.filter.scss')->setPublic(true);
+        }
+        if($container->hasDefinition('assetic.filter_manager')) {
+            $container->getDefinition('assetic.filter_manager')->setPublic(true);
+        }
+        if($container->hasDefinition('assetic.asset_manager')) {
+            $container->getDefinition('assetic.asset_manager')->setPublic(true);
+        }
+        if($container->hasDefinition('victoire_core.entity_proxy.cache_driver')) {
+            $container->getDefinition('victoire_core.entity_proxy.cache_driver')->setPublic(true);
+        }
     }
 }
