1821d7cddafafb37b06e21059704bb2f63b3d631
Leny BERNARD <leny@troopers.email>
2019-10-31 10:54:47 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- Bundle/SitemapBundle/Controller/SitemapController.php
+++ Bundle/SitemapBundle/Controller/SitemapController.php
@@ -2,7 +2,6 @@
 
 namespace Victoire\Bundle\SitemapBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
